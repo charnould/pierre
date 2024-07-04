da5833140e51e237b8a58e543731cf8abce6f3c1
charnould <charnould@icloud.com>
2024-07-04 11:20:03 +0200

    delete some meta tags and preconnect links

--- views/index.ts
+++ views/index.ts
@@ -6,18 +6,11 @@ export const view = (config: Config) => {
     <html lang="fr">
       <head>
         <meta charset="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
         <link rel="stylesheet" href="../assets/_default/design.css" />
         <link rel="stylesheet" href="../assets/${config.id}/design.css" />
         <link rel="manifest" href="../assets/${config.id}/manifest.json" />
         <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
-        <script defer src="../assets/script.js"></script>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <title>PIERRE — Une IA au service du mouvement HLM</title>
+        <script defer src="../assets/_default/scripts/build/ai.js"></script>
       </head>
 
       <body>
