bab225096ace95eeb9eba5681b68654e6c9e6783
charnould <charnould@icloud.com>
2024-07-29 10:59:08 +0200

    Rewrite knowledge build script

--- utils/knowledge/2-Build-knowledge.ts
+++ utils/knowledge/2-Build-knowledge.ts
@@ -2,15 +2,16 @@ import { Database } from 'bun:sqlite'
 import { readdir } from 'node:fs/promises'
 import { openai } from '@ai-sdk/openai'
 import { embed } from 'ai'
+import { generateObject} from 'ai'
 import { $ } from 'bun'
-import { Document, MarkdownNodeParser } from 'llamaindex'
 import * as sqlite_vss from 'sqlite-vss'
+import { z } from 'zod'
 
 console.log(' ')
 console.log('______________')
 console.log(' ')
-console.log('Start building knowledge database')
-console.log('Estimated build time: 10-15 min')
+console.log('START BUILDING KNOWLEDGE DATABASE')
+console.log('(estimated build time: 10-15 min)')
 console.log(' ')
 
 // Delete old knowledge database
@@ -25,53 +26,73 @@ sqlite_vss.load(db)
 // const version = db.prepare('select vss_version()').get()
 // console.log(version)
 
-db.exec('CREATE VIRTUAL TABLE chunks USING FTS5(chunk);')
+db.exec('CREATE VIRTUAL TABLE chunks USING FTS5(chunk, questions);')
 
 const files = await readdir('knowledge', { recursive: true })
 
 for await (const file of files) {
   if (file.endsWith('.md')) {
-    console.log(' 👉 working on: ', file)
+    console.log(' 👉 working on:', file)
 
     const content = Bun.file(`knowledge/${file}`)
     const data = await content.text()
-    const nodeParser = new MarkdownNodeParser()
-    const nodes = nodeParser.getNodesFromDocuments([new Document({ text: data })])
+    const splitted_data = data.split(/[^#]##\s/)
 
-    for await (const node of nodes) {
-      let chunk = ''
+    for (let index = 1; index < splitted_data.length; index++) {
+      const chunk = `${splitted_data[0]}\n## ${splitted_data[index].trim()}`
+      
+      const { object } = await generateObject({
+        model: openai('gpt-4o-mini-2024-07-18'),
+        schema: z.object({
+          questions: z.array(z.string()).describe('Questions')
+        }),
+        mode: 'json',
+        messages: [
+          {
+            role: 'system',
+            content: `
+              ### CHUNK ###
+              ${chunk}
+              
+              ### INSTRUCTION ###
+              List 5-15 questions above chunk answer to
+              Answer only in french language.
+              Return results in JSON.
+              `.trim() // Some LLMs don't allow trailing white space (e.g. Anthropic)
+          }
+        ]
+      })
 
-      for (const key in node.metadata) {
-        if (key === 'Header 1') chunk += `# ${node.metadata[key]}\n`
-        if (key === 'Header 2') chunk += `## ${node.metadata[key]}\n`
-        if (key === 'Header 3') chunk += `### ${node.metadata[key]}\n`
-        if (key === 'Header 4') chunk += `#### ${node.metadata[key]}\n`
-        if (key === 'Header 5') chunk += `##### ${node.metadata[key]}\n`
-        if (key === 'Header 6') chunk += `###### ${node.metadata[key]}\n`
-        if (key === 'Header 7') chunk += `####### ${node.metadata[key]}\n`
+      const { questions } = object
+      let questions_list = ''
+      for (const q of questions) {
+        questions_list += '- ' + q + '\n'
       }
 
-      chunk += node.text // TODO: supprimer la répétition du dernier titre dans le chunk
-
-      db.prepare('INSERT INTO chunks(chunk) VALUES(?);').run(chunk)
+      db.prepare('INSERT INTO chunks(chunk, questions) VALUES(?, ?);').run(chunk, questions_list)
     }
   }
 }
 
-// foreach row, create embedding, and save it with the very same embedding
-db.exec('CREATE VIRTUAL TABLE vectors using vss0(vector(3072));')
+db.exec('CREATE VIRTUAL TABLE vectors using vss0(chunk_vector(3072), questions_vector(3072));')
 const query = db.query('SELECT rowid, * FROM chunks').all()
 
 for await (const q of query) {
-  const { embedding } = await embed({
+  const { embedding: chunk_vector } = await embed({
     model: openai.embedding('text-embedding-3-large'),
     value: q.chunk
   })
 
-  db.prepare('INSERT INTO vectors(rowid, vector) VALUES (?, ?)').run(q.rowid, JSON.stringify(embedding))
+  const { embedding: questions_vector } = await embed({
+    model: openai.embedding('text-embedding-3-large'),
+    value: q.questions
+  })
+
+  db.prepare('INSERT INTO vectors(rowid, chunk_vector, questions_vector) VALUES (?, ?, ?)').run(q.rowid, JSON.stringify(chunk_vector), JSON.stringify(questions_vector))
 }
 
 console.log(' ')
-console.log('End building knowledge database')
+console.log('END BUILDING KNOWLEDGE DATABASE')
+console.log('You can safely close your Terminal')
 console.log('______________')
 console.log(' ')
