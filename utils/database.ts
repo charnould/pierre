5001aa28144558a06b54e2ed72daf352063c2349
charnould <charnould@icloud.com>
2024-10-01 22:28:18 +0200

    refactor: add TS types + check tests + improve all `telemetry` SQLite queries

--- utils/database.ts
+++ utils/database.ts
@@ -1,17 +1,19 @@
 import { Database } from 'bun:sqlite'
 import * as sqlite_vec from 'sqlite-vec'
-// Builtin SQLite library on MacOS doesn't allow extensions!
+// Builtin SQLite library on MacOS doesn't allow extensions
 Database.setCustomSQLite('/opt/homebrew/opt/sqlite/lib/libsqlite3.dylib')
 
-export const db = (db_name: string) => {
-  let db: sqlite_vec.Db | undefined = undefined
-
+export const db = (db_name: 'knowledge' | 'telemetry'): sqlite_vec.Db | Database => {
   if (db_name === 'knowledge') {
-    db = new Database('./utils/knowledge/datastore.sqlite')
-    sqlite_vec.load(db)
+    const knowledge_db: sqlite_vec.Db = new Database('./utils/knowledge/datastore.sqlite')
+    sqlite_vec.load(knowledge_db)
+    return knowledge_db
   }
 
-  if (db_name === 'telemetry') db = new Database('./telemetry/datastore.sqlite')
+  if (db_name === 'telemetry') {
+    const telemetry_db: Database = new Database('./telemetry/datastore.sqlite')
+    return telemetry_db
+  }
 
-  return db
+  throw new Error('Invalid db_name. Database not initialized.')
 }
