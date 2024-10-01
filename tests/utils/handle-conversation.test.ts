4699e7edf7a9bd925f6bbc767c9f28e47ce9ef9e
charnould <charnould@icloud.com>
2024-10-01 22:28:18 +0200

    refactor: add TS types + check tests + improve all `telemetry` SQLite queries

--- tests/utils/handle-conversation.test.ts
+++ tests/utils/handle-conversation.test.ts
@@ -1,16 +1,25 @@
+import { Database } from 'bun:sqlite'
 import { afterAll, beforeAll, expect, it, setSystemTime } from 'bun:test'
 import { AIContext } from '../../utils/_schema'
 import { db } from '../../utils/database'
 import {
+  delete_conversation,
   get_conversation,
   get_conversations_for_review,
   save_reply,
   score_conversation
 } from '../../utils/handle-conversation'
 
+//
+//
+//
+//
+//
+//
 beforeAll(() => {
-  setSystemTime(new Date('2024-12-12T21:12:00.000Z'))
-  db('telemetry').query('DELETE FROM telemetry').run()
+  setSystemTime(new Date('2012-11-10T09:08:07'))
+  const tdb = db('telemetry')
+  if (tdb instanceof Database) tdb.query('DELETE FROM telemetry').run()
 })
 
 //
@@ -19,36 +28,37 @@ beforeAll(() => {
 //
 //
 //
-it('should insert 3 replies and get 2 conversations', async () => {
-  const reply_1 = await AIContext.parseAsync({
-    id: '49bead3b-5e21-4c15-aa25-c8c6ba6ea94c',
+it('should insert 3 replies and retrieve 2 conversations', async () => {
+  // id_1
+  const r_1_1 = await AIContext.parseAsync({
+    id: 'id_1',
     config: 'pierre-ia.org',
     role: 'user',
-    content: 'Bonjour, qui es-tu ?'
+    content: 'Qui es-tu ?'
   })
 
-  save_reply(reply_1, false)
-
-  const reply_2 = await AIContext.parseAsync({
-    id: '49bead3b-5e21-4c15-aa25-c8c6ba6ea94c',
+  const r_1_2 = await AIContext.parseAsync({
+    id: 'id_1',
     config: 'pierre-ia.org',
     role: 'assistant',
-    content: 'Je suis Pierre'
+    content: 'Je suis Pierre !'
   })
 
-  save_reply(reply_2, false)
-
-  expect(get_conversation(reply_2.id)).toMatchSnapshot()
-
-  const reply_3 = await AIContext.parseAsync({
-    id: '6e6f3b06-4754-4ca0-b4ff-0fb941bcb254',
+  // id_2
+  const r_2_1 = await AIContext.parseAsync({
+    id: 'id_2',
     config: 'pierre-ia.org',
     role: 'user',
     content: 'Bonjour'
   })
 
-  save_reply(reply_3, false)
-  expect(get_conversation(reply_3.id)).toMatchSnapshot()
+  save_reply(r_1_1, false)
+  save_reply(r_1_2, false)
+  save_reply(r_2_1, false)
+
+  // Tests
+  expect(get_conversation(r_1_1.id)).toMatchSnapshot()
+  expect(get_conversation(r_2_1.id)).toMatchSnapshot()
 })
 
 //
@@ -57,28 +67,39 @@ it('should insert 3 replies and get 2 conversations', async () => {
 //
 //
 //
-it('should score a conversation for user and reviewer', () => {
+it('should score 2 conversations for customer + organization + external', () => {
   score_conversation(
     {
-      id: '49bead3b-5e21-4c15-aa25-c8c6ba6ea94c',
-      scorer: 'user',
+      id: 'id_1',
+      scorer: 'customer',
       score: 0,
-      comment: null
+      comment: "C'est nul pour le Customer"
     },
     false
   )
 
   score_conversation(
     {
-      id: '49bead3b-5e21-4c15-aa25-c8c6ba6ea94c',
-      scorer: 'reviewer',
-      score: 2,
-      comment: 'Réponse peu précise'
+      id: 'id_1',
+      scorer: 'organization',
+      score: 1,
+      comment: "C'est 1 pour l'Organzation"
     },
     false
   )
 
-  expect(get_conversation('49bead3b-5e21-4c15-aa25-c8c6ba6ea94c')).toMatchSnapshot()
+  score_conversation(
+    {
+      id: 'id_2',
+      scorer: 'external',
+      score: 2,
+      comment: "C'est 2 pour l'External"
+    },
+    false
+  )
+
+  expect(get_conversation('id_1')).toMatchSnapshot()
+  expect(get_conversation('id_2')).toMatchSnapshot()
 })
 
 //
@@ -87,8 +108,44 @@ it('should score a conversation for user and reviewer', () => {
 //
 //
 //
-it('should get all conversations', () => {
+it('should insert 2 replies (from 1 conversation) and delete this conversation', async () => {
+  // id_3
+  const r_3_1 = await AIContext.parseAsync({
+    id: 'id_3',
+    config: 'pierre-ia.org',
+    role: 'user',
+    content: 'Conv 3_1'
+  })
+
+  const r_3_2 = await AIContext.parseAsync({
+    id: 'id_3',
+    config: 'pierre-ia.org',
+    role: 'assistant',
+    content: 'Conv 3_2'
+  })
+
+  save_reply(r_3_1, false)
+  save_reply(r_3_2, false)
+
+  // Tests
+  expect(get_conversation(r_3_1.id)).toMatchSnapshot()
+  expect(delete_conversation(r_3_1.id)).toMatchSnapshot()
+})
+
+//
+//
+//
+//
+//
+//
+it('should retrieve all conversations', () => {
   expect(get_conversations_for_review()).toMatchSnapshot()
 })
 
+//
+//
+//
+//
+//
+//
 afterAll(() => setSystemTime())
--- utils/handle-conversation.ts
+++ utils/handle-conversation.ts
@@ -1,58 +1,77 @@
+import { Database } from 'bun:sqlite'
+import { format } from 'date-fns'
 import { db } from '../utils/database'
 import type { AIContext, Reply } from './_schema'
 
 //
 //
-//
-//
-//
-export const get_conversation = (id: string): Reply[] =>
-  db('telemetry')
-    .prepare('SELECT * FROM telemetry WHERE id = $id ORDER BY timestamp ASC')
-    .all({ $id: id })
+// Get ONE conversation
+// Test: OK
+export const get_conversation = (conv_id: string) => {
+  const tdb = db('telemetry')
+
+  if (tdb instanceof Database) {
+    return tdb
+      .prepare('SELECT * FROM telemetry WHERE id = $id ORDER BY timestamp ')
+      .all({ $id: conv_id }) as Reply[]
+  }
+
+  throw new Error('Invalid database type for telemetry_db')
+}
 
 //
 //
-//
-//
-//
-export const delete_conversation = (id: string) =>
-  db('telemetry').prepare('DELETE FROM telemetry WHERE id = $id').run({ $id: id })
+// Delete ONE conversation
+// Test : OK
+export const delete_conversation = (conv_id: string) => {
+  const tdb = db('telemetry')
+
+  if (tdb instanceof Database) {
+    return tdb.prepare('DELETE FROM telemetry WHERE id = $id').run({ $id: conv_id })
+  }
+
+  throw new Error('Invalid database type for telemetry_db')
+}
 
 //
 //
-//
-//
-//
+// Save ONE reply (a single part of a conversation)
+// Test: OK
+// TODO: Test "outside telemetry"
 export const save_reply = async (context: AIContext, telemetry: boolean) => {
   try {
-    db('telemetry')
-      .prepare(
-        `INSERT INTO telemetry (
-        id,
-        config,
-        model,
-        role,
-        content,
-        timestamp,
-        prompt_tokens,
-        completion_tokens,
-        total_tokens
-      ) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
-      )
-      .run([
-        context.id,
-        context.config.id,
-        context.model,
-        context.role,
-        context.content,
-        new Date().toISOString(),
-        context.usage.prompt_tokens,
-        context.usage.completion_tokens,
-        context.usage.total_tokens
-      ])
+    const telemetry_db = db('telemetry')
 
+    if (telemetry_db instanceof Database && typeof context.config !== 'string') {
+      telemetry_db
+        .prepare(
+          `INSERT INTO telemetry (
+            id,
+            config,
+            model,
+            role,
+            content,
+            timestamp,
+            prompt_tokens,
+            completion_tokens,
+            total_tokens
+            ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
+        )
+        .run(
+          context.id,
+          context.config.id,
+          context.config.model,
+          context.role,
+          context.content,
+          format(new Date(), "yyyy-MM-dd'T'HH:mm:ssXXX"),
+          context.usage.prompt_tokens,
+          context.usage.completion_tokens,
+          context.usage.total_tokens
+        )
+    }
+
+    // Telemetry
     if (Bun.env.TELEMETRY === 'true' && telemetry === true) {
       await fetch('https://pierre-ia.org/telemetry', {
         method: 'POST',
@@ -66,23 +85,30 @@ export const save_reply = async (context: AIContext, telemetry: boolean) => {
 
 //
 //
-//
-//
-//
+// Score ONE conversation (= all parts/replies)
+// Test: ok
+// TODO: Test "outside telemetry"
 export const score_conversation = async ({ id, scorer, score, comment }, telemetry: boolean) => {
   try {
-    db('telemetry')
-      .prepare(
-        ` UPDATE telemetry
-          SET
-            ${scorer === 'reviewer' ? 'reviewer_score = $score ,' : ''}
-            ${scorer === 'reviewer' ? 'reviewer_comment = $comment' : ''}
-            ${scorer === 'user' ? 'user_score = $score' : ''}    
-        WHERE id = $id `
-      )
+    const tdb = db('telemetry')
 
-      .run({ $id: id, $score: score, $comment: comment })
+    if (tdb instanceof Database) {
+      tdb
+        .prepare(
+          `UPDATE telemetry
+           SET
+            ${scorer === 'customer' ? 'cus_satisfaction = $score ,' : ''}
+            ${scorer === 'customer' ? 'cus_comment = $comment' : ''}
+            ${scorer === 'external' ? 'ext_satisfaction = $score ,' : ''}
+            ${scorer === 'external' ? 'ext_comment = $comment' : ''}
+            ${scorer === 'organization' ? 'org_satisfaction = $score ,' : ''}
+            ${scorer === 'organization' ? 'org_comment = $comment' : ''}
+          WHERE id = $id `
+        )
+        .run({ $id: id, $score: score, $comment: comment })
+    }
 
+    // Telemetry
     if (Bun.env.TELEMETRY === 'true' && telemetry === true) {
       await fetch('https://pierre-ia.org/telemetry', {
         method: 'POST',
@@ -102,8 +128,14 @@ export const score_conversation = async ({ id, scorer, score, comment }, telemet
 
 //
 //
-//
-//
-//
-export const get_conversations_for_review = (): Reply[] =>
-  db('telemetry').prepare('SELECT * FROM telemetry ORDER BY timestamp DESC').all()
+// Get ALL conversations for review (./eval)
+// Test: OK
+export const get_conversations_for_review = (): Reply[] => {
+  const tdb = db('telemetry')
+
+  if (tdb instanceof Database) {
+    return tdb.prepare('SELECT * FROM telemetry ORDER BY timestamp DESC').all() as Reply[]
+  }
+
+  throw new Error('Invalid database type for telemetry_db')
+}
