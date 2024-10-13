6feb9fec7463734dd485bbc5d3573440b62be625
charnould <charnould@icloud.com>
2024-10-13 18:17:03 +0200

    feat: revamp SQLite data model

--- config/telemetry.sh
+++ config/telemetry.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -e
 
 init_db () {
-     mkdir -p telemetry
-     sqlite3 telemetry/datastore.sqlite "CREATE TABLE IF NOT EXISTS telemetry (conv_id TEXT, config TEXT, model TEXT, role TEXT, timestamp TEXT, content TEXT, cus_satisfaction INTEGER, org_satisfaction INTEGER, ext_satisfaction INTEGER, cus_comment TEXT, org_comment TEXT, ext_comment TEXT, prompt_tokens INTEGER, completion_tokens INTEGER, total_tokens INTEGER);";
-     }
+    
+    # Ensure that `telemetry` folder exists.
+    # If `telemetry` directory already exists, it won’t raise any errors.
+    mkdir -p telemetry
 
-reset_db () {
-    sqlite3 telemetry/datastore.sqlite "DELETE FROM telemetry; VACUUM;";
-     }
+    # Create `datastore.sqlite` if it does not exist,
+    # and create `telemetry` table with schema below.
+    sqlite3 telemetry/datastore.sqlite <<EOF
+
+        CREATE TABLE IF NOT EXISTS telemetry (
+            conv_id TEXT,
+            config TEXT,
+            role TEXT,
+            timestamp TEXT,
+            content TEXT,
+            metadata TEXT
+        );
+EOF
+}
 
  "$@"
