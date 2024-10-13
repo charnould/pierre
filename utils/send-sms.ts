171b68f0d6ce94aa2bc4cc1f6fcf6d3e7efd5295
charnould <charnould@icloud.com>
2024-10-13 14:58:15 +0200

    fix: split sms into part if it's too long for CM.com

--- utils/send-sms.ts
+++ utils/send-sms.ts
@@ -1,30 +1,37 @@
+import { split_sms } from './split-sms'
+
 export const send_sms = async ({ from, to, message }) => {
   try {
-    // https://developers.cm.com/messaging/docs/integration
-    const response = await fetch('https://gw.messaging.cm.com/v1.0/message', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      },
-      body: JSON.stringify({
-        messages: {
-          authentication: { productToken: Bun.env.SMS_API_KEY },
-          msg: [
-            {
-              from: from,
-              to: [{ number: to }],
-              body: { type: 'auto', content: message },
-              reference: 'n/a'
-            }
-          ]
-        }
+    const sms = split_sms(message)
+    for (const part of sms) {
+      // https://developers.cm.com/messaging/docs/integration
+      const response = await fetch('https://gw.messaging.cm.com/v1.0/message', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          messages: {
+            authentication: { productToken: Bun.env.SMS_API_KEY },
+            msg: [
+              {
+                from: from,
+                to: [{ number: to }],
+                body: { type: 'auto', content: part },
+                reference: 'none'
+              }
+            ]
+          }
+        })
       })
-    })
 
-    const data = await response.json()
-    data.message = message
-    console.log('###### CM.com response ######\n', data)
+      const data = await response.json()
+      data.message = message
+      console.log('###### CM.com response ######\n', data)
+
+      await Bun.sleep(1250) // Sleep to avoid CM.com error
+    }
   } catch (error) {
     console.error(error)
   }
