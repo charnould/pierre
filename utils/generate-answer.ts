8ba04ad32b1e6ddba2208030c5247381d4190323
charnould <charnould@icloud.com>
2024-10-25 21:50:08 +0200

    refactor: rewrite deadlock prompts (+ align to new schema)

--- utils/generate-answer.ts
+++ utils/generate-answer.ts
@@ -3,12 +3,6 @@ import type { AIContext } from './_schema'
 import { generate_answer, stream_answer } from './deliver-answer'
 
 //
-// ██╗   ██╗███████╗███████╗██████╗      █████╗ ███████╗██╗  ██╗███████╗    ███████╗ ██████╗ ██████╗
-// ██║   ██║██╔════╝██╔════╝██╔══██╗    ██╔══██╗██╔════╝██║ ██╔╝██╔════╝    ██╔════╝██╔═══██╗██╔══██╗
-// ██║   ██║███████╗█████╗  ██████╔╝    ███████║███████╗█████╔╝ ███████╗    █████╗  ██║   ██║██████╔╝
-// ██║   ██║╚════██║██╔══╝  ██╔══██╗    ██╔══██║╚════██║██╔═██╗ ╚════██║    ██╔══╝  ██║   ██║██╔══██╗
-// ╚██████╔╝███████║███████╗██║  ██║    ██║  ██║███████║██║  ██╗███████║    ██║     ╚██████╔╝██║  ██║
-//  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝    ╚═╝      ╚═════╝ ╚═╝  ╚═╝
 //  ██████╗██╗     ███████╗██╗   ██╗███████╗██████╗ ███╗   ██╗███████╗███████╗███████╗
 // ██╔════╝██║     ██╔════╝██║   ██║██╔════╝██╔══██╗████╗  ██║██╔════╝██╔════╝██╔════╝
 // ██║     ██║     █████╗  ██║   ██║█████╗  ██████╔╝██╔██╗ ██║█████╗  ███████╗███████╗
@@ -55,7 +49,7 @@ Maintain a professional and supportive tone, appropriate for engaging with indiv
 
 # RESPONSE #
 
-- The response must be in ${context.lang}.
+- The response must be in ${context.query.lang}.
 - Ensure answers are brief when appropriate but provide explanations where required for clarity.
 
 # CONTEXT #
@@ -64,7 +58,7 @@ ${context.chunks}
 
 ---
 
-QUESTION: ${context.translated_followup}
+QUESTION: ${context.content}
 YOUR ANSWER:
 `.trim() // Some LLMs don't allow trailing white space (e.g. Anthropic)
   })
@@ -75,73 +69,60 @@ YOUR ANSWER:
 }
 
 //
-// ██╗   ██╗███████╗███████╗██████╗     ██████╗ ███████╗ █████╗  ██████╗██╗  ██╗███████╗███████╗
-// ██║   ██║██╔════╝██╔════╝██╔══██╗    ██╔══██╗██╔════╝██╔══██╗██╔════╝██║  ██║██╔════╝██╔════╝
-// ██║   ██║███████╗█████╗  ██████╔╝    ██████╔╝█████╗  ███████║██║     ███████║█████╗  ███████╗
-// ██║   ██║╚════██║██╔══╝  ██╔══██╗    ██╔══██╗██╔══╝  ██╔══██║██║     ██╔══██║██╔══╝  ╚════██║
-// ╚██████╔╝███████║███████╗██║  ██║    ██║  ██║███████╗██║  ██║╚██████╗██║  ██║███████╗███████║
-//  ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝
-//  █████╗     ██████╗ ███████╗ █████╗ ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗
-// ██╔══██╗    ██╔══██╗██╔════╝██╔══██╗██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝
-// ███████║    ██║  ██║█████╗  ███████║██║  ██║██║     ██║   ██║██║     █████╔╝
-// ██╔══██║    ██║  ██║██╔══╝  ██╔══██║██║  ██║██║     ██║   ██║██║     ██╔═██╗
-// ██║  ██║    ██████╔╝███████╗██║  ██║██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
+// ██████╗ ███████╗ █████╗ ██████╗ ██╗      ██████╗  ██████╗██╗  ██╗
+// ██╔══██╗██╔════╝██╔══██╗██╔══██╗██║     ██╔═══██╗██╔════╝██║ ██╔╝
+// ██║  ██║█████╗  ███████║██║  ██║██║     ██║   ██║██║     █████╔╝
+// ██║  ██║██╔══╝  ██╔══██║██║  ██║██║     ██║   ██║██║     ██╔═██╗
+// ██████╔╝███████╗██║  ██║██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
 //
 
 export const reach_deadlock = async (context: AIContext, options: { is_sms: boolean }) => {
   // Start with a prompt containing only chatbot persona
-  let prompt = `# YOUR PERSONA #\n\n${(context.config as { persona: string }).persona}\n\n`
+  let prompt = `
+
+# YOUR PERSONA #
+
+${(context.config as { persona: string }).persona}`
 
   // If context DOES contain profanity
-  if (context.contains_profanity === true) {
+  if (context.query?.contains_profanity) {
     prompt += `
-# OBJECTIVE #
 
-- Politely explain that, even if your an AI, only respectful communication will be answered.
-- Politely explain also you are here to help according to your persona.
+# INSTRUCTION
 
-# TONE #
-
-- Respond in a formal and respectful tone at all times.
-- Use formal, professional yet accessible tone.
-
-# RESPONSE #
-
-- The response must be in ${context.lang} ${context.lang === 'french' ? '(always use the formal "vous")' : ''}.
-- The response must **not** contain smileys or emojis.
+- You are a polite and respectful AI assistant.
+- Respond with a calm, friendly reminder that you are here to help but will only respond to polite, respectful conversation.
+- Encourage the user to rephrase their question courteously, as mutual respect is key to effective communication.
+- Always respond in a positive, understanding tone that keeps the conversation constructive.
+- Omit emoticons, emojis, or informal punctuation.
+- Explain you are here to help according to your [persona].
 
 ---
-YOUR ANSWER:`
+
+Your answer in ${context.query.lang} (ISO 639-1 format):`
   }
 
   // If context DOES NOT contain profanity
-  if (context.contains_profanity === false) {
-    prompt += '# OBJECTIVE #\n\n'
-
-    if (context.is_greeting === true) {
-      prompt +=
-        '- Respond to the greeting in an **enthusiastic and friendly** manner in a few words.\n'
-    }
-    if (context.is_about_yourself === true) {
-      prompt +=
-        '- Provide a **brief and professional introduction** about yourself in 30-50 words.\n'
-    }
-    if (context.is_about_housing === false) {
-      prompt += `- Answer user question in 10-30 words and explain that while you have general knowledge about the user's query, you are **specifically designed to answer only questions about housing**.\n`
-    }
-
+  else {
     prompt += `
+    
+# INSTRUCTIONS
 
-# YOUR RESPONSE #
+- You are a courteous and focused assistant.
+- Always use context from previous messages.
+- Avoid repetitive disclaimers.
+- Language-specific considerations: formal "vous" in French.
 
-- The response must be in ${context.lang} ${context.lang === 'french' ? '(always use the formal "vous")' : ''}.
-- The response must **not** contain smileys or emojis.
-- ${options.is_sms === true ? 'Ensure response is suited for SMS interactions.' : 'Format your answer to improve readability.'}
+- Respond briefly in one or two sentences.
+- However if a question is about yourself or your capabilities, respond openly and in detail as appropriate.
+- Then follow up by:
+  - Reminding user what kind of question you can ONLY answer.
+  - Encouraging user to ask questions where you can be most effective.
 
 ---
 
-QUESTION: ${context.translated_followup}
-YOUR ANSWER:`
+Question: ${context.content}
+Your answer in ${context.query?.lang}:`
   }
 
   // Add prompt to conversation history
