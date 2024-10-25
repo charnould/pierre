c8f63813b40615d289dd939a46575ba5331d53c3
charnould <charnould@icloud.com>
2024-10-25 15:17:27 +0200

    refactor: rewrite/refactor `GET.ai` (to make it works with new query augmentation)

--- controllers/GET.ai.ts
+++ controllers/GET.ai.ts
@@ -1,8 +1,8 @@
 import type { CoreTool, StreamTextResult } from 'ai'
 import type { Context } from 'hono'
 import { streamText } from 'hono/streaming'
-import { AIContext, type SMS } from '../utils/_schema'
-import { enhance_query } from '../utils/enhance-query'
+import { AIContext, type Config, type SMS } from '../utils/_schema'
+import { augment_query } from '../utils/augment-query'
 import { answer_user, reach_deadlock } from '../utils/generate-answer'
 import { get_conversation, save_reply } from '../utils/handle-conversation'
 import { parse_incoming_sms } from '../utils/parse-incoming-sms'
@@ -12,31 +12,22 @@ import { send_sms } from '../utils/send-sms'
 
 export const controller = async (c: Context) => {
   try {
-    //
-    // Set variables + TS types
+    // Step 1. Set variables and types
     let is_sms = false
     let parsed_sms: SMS = null
-    let context: AIContext | undefined
-    let answer: StreamTextResult<Record<string, CoreTool>> | string = ''
+    let context: AIContext
+    let answer: string | StreamTextResult<Record<string, CoreTool>>
 
-    //
-    // Step 1.
-    // Check if incoming request is a valid SMS
+    // Step 2. Check if incoming request is a valid SMS
     if (c.req.path === '/sms') {
-      const json = await c.req.json()
-      parsed_sms = await parse_incoming_sms(json)
-      if (parsed_sms === null) is_sms = false
-      else is_sms = true
+      parsed_sms = await parse_incoming_sms(await c.req.json())
+      if (parsed_sms !== null) is_sms = true
     }
 
-    //
-    // Step 2
-    //
-    // If incoming conversation comes from a SMS
-    if (is_sms === true) context = await AIContext.parseAsync(parsed_sms)
-
-    // If incoming conversation comes from WWW
-    if (is_sms === false) {
+    // Step 3. Create/parse an answer context
+    if (is_sms) {
+      context = await AIContext.parseAsync(parsed_sms)
+    } else {
       context = await AIContext.parseAsync({
         role: 'user',
         conv_id: c.req.param('id'),
@@ -45,89 +36,70 @@ export const controller = async (c: Context) => {
       })
     }
 
-    //
-    // Step 3.
-    // Apply all IA and retrieval logic...!
-    if (context) {
-      //
-      // 1. Save user query in DB
-      // 2. Get the ALL conversation and assign it
-      // 3. Parse and enhance user query
-      save_reply(context, true)
-      context.conversation = get_conversation(context.conv_id)
-      context = await AIContext.parseAsync({ ...context, ...(await enhance_query(context)) })
+    // Step 4. Apply AI logic
 
-      // If query/request is not good, prepare a deadlock answer
-      if (
-        (context.is_about_housing === false && context.is_about_yourself === true) ||
-        (context.is_about_housing === false && context.is_greeting === true) ||
-        context.is_about_housing === false ||
-        context.contains_profanity === true
-      ) {
-        answer = await reach_deadlock(context, { is_sms: is_sms })
-      } else {
-        //
-        // If query/request is good,
-        // retrieve the best possible chunks...
-        const chunks = await Promise.all([
-          vector_search(context.content),
-          vector_search(context.stepback),
-          vector_search((context.hyde as string[])[0]),
-          vector_search((context.hyde as string[])[1]),
-          vector_search((context.hyde as string[])[2]),
-          vector_search((context.queries as string[])[0]),
-          vector_search((context.queries as string[])[1]),
-          vector_search((context.queries as string[])[2])
-        ])
+    save_reply(context, true)
 
-        // Deduplicate and rank them
-        context.chunks = rank_chunks(chunks)
+    context = await AIContext.parseAsync({
+      ...context,
+      conversation: get_conversation(context.conv_id)
+    }).then(async (context) => ({ ...context, query: await augment_query(context) }))
 
-        // Finally, generate the answer
-        answer = await answer_user(context, { is_sms: is_sms })
-      }
+    // If query is relevant and does not contain profanity, answer with intelligence
+    if (context.query?.is_relevant && !context.query.contains_profanity) {
+      console.debug('🤖 should answer with intelligence')
+
+      const chunks = await Promise.all(
+        [
+          ...context.query.standalone_questions,
+          ...context.query.stepback_questions,
+          ...context.query.search_queries,
+          ...context.query.hyde_answers
+        ].map((q) => vector_search(q))
+      )
+
+      context.chunks = rank_chunks(chunks)
+
+      console.debug('🤖 augments the user query to:\n', context)
+
+      answer = (await answer_user(context, { is_sms: is_sms })) as
+        | string
+        | StreamTextResult<Record<string, CoreTool>>
+    } else {
+      // If query is irrelevant and/or contains profanity, make a deadlock answer
+      console.debug('🤖 should answer with a deadlock')
+
+      answer = (await reach_deadlock(context, { is_sms: is_sms })) as
+        | string
+        | StreamTextResult<Record<string, CoreTool>>
     }
 
-    //
-    // If incoming conversation comes from:
-    //
-    // ███████╗███╗   ███╗███████╗
-    // ██╔════╝████╗ ████║██╔════╝
-    // ███████╗██╔████╔██║███████╗
-    // ╚════██║██║╚██╔╝██║╚════██║
-    // ███████║██║ ╚═╝ ██║███████║
-    // ╚══════╝╚═╝     ╚═╝╚══════╝
-    //
-    // return a full-text answer:
-    //
+    // Step 5. Send answer to the user
 
-    if (is_sms === true) {
-      await send_sms({ from: parsed_sms?.phone, to: parsed_sms?.to, message: answer })
+    // If incoming request is a SMS, return a full-text answer
+    if (is_sms && parsed_sms !== null) {
+      await send_sms({
+        from: (context.config as Config).phone,
+        to: parsed_sms.to,
+        message: answer
+      })
+
       return c.body('ok', 200)
     }
 
-    //
-    // If incoming conversation comes from:
-    //
-    // ██╗    ██╗██╗    ██╗██╗    ██╗
-    // ██║    ██║██║    ██║██║    ██║
-    // ██║ █╗ ██║██║ █╗ ██║██║ █╗ ██║
-    // ██║███╗██║██║███╗██║██║███╗██║
-    // ╚███╔███╔╝╚███╔███╔╝╚███╔███╔╝
-    //
-    // return a stream:
-    //
+    // If incoming request comes from the WWW, return a text stream
 
-    if (is_sms === false) {
-      return streamText(c, async (stream) => {
-        c.header('Content-Type', 'text/event-stream')
-        for await (const textPart of answer.textStream) {
-          await stream.write(`data: ${textPart.replace(/\n/g, '<br/>')}\n\n` ?? '')
-        }
-        await stream.write('data: pierre_event_stream_closed\n\n')
-      })
-    }
-  } catch (err) {
-    console.error(err)
+    // TODO: optimize stream (see. Hono/Vercel SDK Core)
+    return streamText(c, async (stream) => {
+      c.header('Content-Type', 'text/event-stream')
+
+      for await (const textPart of answer.textStream) {
+        await stream.write(`data: ${textPart.replace(/\n/g, '<br/>')}\n\n` ?? '')
+      }
+
+      await stream.write('data: pierre_event_stream_closed\n\n')
+    })
+  } catch (e) {
+    console.error(e)
   }
 }
