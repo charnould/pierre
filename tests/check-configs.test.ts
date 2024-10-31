741201c8cb5da756b99c6d2a1865020b9835cd2a
charnould <charnould@icloud.com>
2024-10-31 09:29:56 +0100

    test: check whether `config.example.ts` parses successfully

--- tests/check-configs.test.ts
+++ tests/check-configs.test.ts
@@ -5,9 +5,17 @@ import { Config } from '../utils/_schema'
 test('check if configs parse successfully', async () => {
   const directories = await readdir('./assets')
 
+  // Check official `config.ts` for each organization
+  // (These files are git-ignored.)
   for await (const directory of directories) {
     const config = (await import(`../assets/${directory}/config`)).default
     const check = Config.safeParse(config)
     expect(check.success).toBe(true)
   }
+
+  // Check `config.example.ts` in `pierre-ia.org` folder
+  // (This file is committed.)
+  const config = (await import('../assets/pierre-ia.org/config.example')).default
+  const check = Config.safeParse(config)
+  expect(check.success).toBe(true)
 })
