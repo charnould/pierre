046a7f6cea976173a47234712018313a6377a3f0
charnould <charnould@icloud.com>
2024-10-31 22:08:33 +0100

    test: fix tests (since `assets/*/config.ts` is git-ignored)

--- tests/utils/handle-conversation.test.ts
+++ tests/utils/handle-conversation.test.ts
@@ -10,163 +10,167 @@ import {
   score_conversation
 } from '../../utils/handle-conversation'
 
-//
-//
-// Dummy replies for testing purpose
-const r_1_1 = await AIContext.parseAsync({
-  conv_id: 'id_1',
-  config: 'pierre-ia.org',
-  role: 'user',
-  content: 'Qui es-tu ?'
-})
+// This test will fail in GitHub Actions because `config.ts` (via Zod parsing)
+// is not committed. Therefore, it should NOT run in GitHub Actions.
+if (Bun.env.GITHUB_ACTIONS === undefined) {
+  //
+  //
+  // Dummy replies for testing purpose
+  const r_1_1 = await AIContext.parseAsync({
+    conv_id: 'id_1',
+    config: 'pierre-ia.org',
+    role: 'user',
+    content: 'Qui es-tu ?'
+  })
 
-const r_1_2 = await AIContext.parseAsync({
-  conv_id: 'id_1',
-  config: 'pierre-ia.org',
-  role: 'assistant',
-  content: 'Je suis Pierre !'
-})
+  const r_1_2 = await AIContext.parseAsync({
+    conv_id: 'id_1',
+    config: 'pierre-ia.org',
+    role: 'assistant',
+    content: 'Je suis Pierre !'
+  })
 
-const r_2_1 = await AIContext.parseAsync({
-  conv_id: 'id_2',
-  config: 'pierre-ia.org',
-  role: 'user',
-  content: 'Bonjour'
-})
+  const r_2_1 = await AIContext.parseAsync({
+    conv_id: 'id_2',
+    config: 'pierre-ia.org',
+    role: 'user',
+    content: 'Bonjour'
+  })
 
-//
-//
-//
-//
-//
-//
-beforeAll(() => {
-  const database = db('telemetry')
-  if (database instanceof Database) database.query('DELETE FROM telemetry').run()
-})
+  //
+  //
+  //
+  //
+  //
+  //
+  beforeAll(() => {
+    const database = db('telemetry')
+    if (database instanceof Database) database.query('DELETE FROM telemetry').run()
+  })
 
-afterAll(() => {
-  setSystemTime()
-})
+  afterAll(() => {
+    setSystemTime()
+  })
 
-//
-//
-//
-//
-//
-//
-it('should insert 3 replies', async () => {
-  setSystemTime(new Date('2012-12-12T12:05:00'))
-  save_reply(r_1_1, false)
-  setSystemTime(new Date('2012-12-12T12:10:00'))
-  save_reply(r_1_2, false)
-  setSystemTime(new Date('2012-12-12T12:15:00'))
-  save_reply(r_2_1, false)
+  //
+  //
+  //
+  //
+  //
+  //
+  it('should insert 3 replies', async () => {
+    setSystemTime(new Date('2012-12-12T12:05:00'))
+    save_reply(r_1_1, false)
+    setSystemTime(new Date('2012-12-12T12:10:00'))
+    save_reply(r_1_2, false)
+    setSystemTime(new Date('2012-12-12T12:15:00'))
+    save_reply(r_2_1, false)
 
-  const database = db('telemetry')
-  if (database instanceof Database)
-    expect(database.query('SELECT * FROM telemetry').all()).toMatchSnapshot()
-})
+    const database = db('telemetry')
+    if (database instanceof Database)
+      expect(database.query('SELECT * FROM telemetry').all()).toMatchSnapshot()
+  })
 
-//
-//
-//
-//
-//
-//
-it('should retrieve 2 conversations', async () => {
-  // Tests
-  expect(get_conversation(r_1_1.conv_id)).toMatchSnapshot()
-  expect(get_conversation(r_2_1.conv_id)).toMatchSnapshot()
-})
+  //
+  //
+  //
+  //
+  //
+  //
+  it('should retrieve 2 conversations', async () => {
+    // Tests
+    expect(get_conversation(r_1_1.conv_id)).toMatchSnapshot()
+    expect(get_conversation(r_2_1.conv_id)).toMatchSnapshot()
+  })
 
-//
-//
-//
-//
-//
-//
-it('should score conversations', () => {
-  score_conversation(
-    {
-      conv_id: 'id_1',
-      scorer: 'customer',
-      score: 1,
-      comment: 'customer comment'
-    },
-    false
-  )
+  //
+  //
+  //
+  //
+  //
+  //
+  it('should score conversations', () => {
+    score_conversation(
+      {
+        conv_id: 'id_1',
+        scorer: 'customer',
+        score: 1,
+        comment: 'customer comment'
+      },
+      false
+    )
 
-  score_conversation(
-    {
-      conv_id: 'id_1',
-      scorer: 'organization',
-      score: 2,
-      comment: 'organization comment'
-    },
-    false
-  )
+    score_conversation(
+      {
+        conv_id: 'id_1',
+        scorer: 'organization',
+        score: 2,
+        comment: 'organization comment'
+      },
+      false
+    )
 
-  score_conversation(
-    {
-      conv_id: 'id_1',
-      scorer: 'pierre',
-      score: 3,
-      comment: 'pierre comment'
-    },
-    false
-  )
+    score_conversation(
+      {
+        conv_id: 'id_1',
+        scorer: 'pierre',
+        score: 3,
+        comment: 'pierre comment'
+      },
+      false
+    )
 
-  score_conversation(
-    {
-      conv_id: 'id_1',
-      scorer: 'ai',
-      score: 4,
-      comment: 'ai comment'
-    },
-    false
-  )
+    score_conversation(
+      {
+        conv_id: 'id_1',
+        scorer: 'ai',
+        score: 4,
+        comment: 'ai comment'
+      },
+      false
+    )
 
-  expect(get_conversation('id_1')).toMatchSnapshot()
+    expect(get_conversation('id_1')).toMatchSnapshot()
 
-  score_conversation(
-    {
-      conv_id: 'id_2',
-      scorer: 'ai',
-      score: 5,
-      comment: 'outstanding answer!'
-    },
-    false
-  )
+    score_conversation(
+      {
+        conv_id: 'id_2',
+        scorer: 'ai',
+        score: 5,
+        comment: 'outstanding answer!'
+      },
+      false
+    )
 
-  expect(get_conversation('id_2')).toMatchSnapshot()
-})
+    expect(get_conversation('id_2')).toMatchSnapshot()
+  })
 
-//
-//
-//
-//
-//
-//
-it('should delete a full conversation', async () => {
-  delete_conversation(r_1_1.conv_id)
-  expect(get_conversation(r_1_1.conv_id)).toStrictEqual([])
-})
+  //
+  //
+  //
+  //
+  //
+  //
+  it('should delete a full conversation', async () => {
+    delete_conversation(r_1_1.conv_id)
+    expect(get_conversation(r_1_1.conv_id)).toStrictEqual([])
+  })
 
-//
-//
-//
-//
-//
-//
-it('should retrieve all conversations', () => {
-  expect(get_conversations()).toMatchSnapshot()
-})
+  //
+  //
+  //
+  //
+  //
+  //
+  it('should retrieve all conversations', () => {
+    expect(get_conversations()).toMatchSnapshot()
+  })
 
-//
-//
-//
-//
-//
-//
-afterAll(() => setSystemTime())
+  //
+  //
+  //
+  //
+  //
+  //
+  afterAll(() => setSystemTime())
+}
