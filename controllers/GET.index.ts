6b649e8dd9b6af4fc3b81c5d447a77aed3606036
charnould <charnould@icloud.com>
2024-11-02 10:47:48 +0100

    refactor: replace `crypto.randomUUID()` with new `randomUUIDv7()` (requires Bun `1.1.34`)

--- controllers/GET.index.ts
+++ controllers/GET.index.ts
@@ -1,3 +1,4 @@
+import { randomUUIDv7 } from 'bun'
 import type { Context } from 'hono'
 import { z } from 'zod'
 import type { Config } from '../utils/_schema'
@@ -6,10 +7,17 @@ import { view } from '../views/index'
 export const controller = async (c: Context) => {
   try {
     const config: Config = (await import(`../assets/${c.req.query('config')}/config`)).default
-    z.string().uuid().parse(c.req.param('id'))
-    return c.html(view(config))
-  } catch (e) {
-    console.log(e)
-    return c.notFound()
+
+    const has_valid_uuid = z.string().uuid().safeParse(c.req.param('id')).success
+    const scenario_query = c.req.query('scenario')
+    const has_valid_scenario = scenario_query ? Boolean(config.scenario[scenario_query]) : false
+
+    if (has_valid_uuid && has_valid_scenario) {
+      return c.html(view(config, scenario_query as string))
+    }
+    const scenario = has_valid_scenario ? scenario_query : 'default'
+    return c.redirect(`/c/${randomUUIDv7()}?config=${config.id}&scenario=${scenario}`)
+  } catch {
+    return c.redirect(`/c/${randomUUIDv7()}?config=pierre-ia.org`)
   }
 }
