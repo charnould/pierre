057994c4e8b7843dd61067b72df231a310808a24
charnould <charnould@icloud.com>
2024-11-09 10:48:40 +0100

    feat: add `users` table to `telemetry` database

--- config/telemetry.sh
+++ config/telemetry.sh
@@ -6,8 +6,7 @@ init_db () {
     # If `telemetry` directory already exists, it won’t raise any errors.
     mkdir -p telemetry
 
-    # Create `datastore.sqlite` if it does not exist,
-    # and create `telemetry` table with schema below.
+    # Create `datastore.sqlite` if it does not exist.
     sqlite3 telemetry/datastore.sqlite <<EOF
 
         CREATE TABLE IF NOT EXISTS telemetry (
@@ -18,6 +17,13 @@ init_db () {
             content TEXT,
             metadata TEXT,
             UNIQUE(conv_id, timestamp)
+        );
+        
+        CREATE TABLE IF NOT EXISTS users (
+            config TEXT NOT NULL,
+            email TEXT PRIMARY KEY UNIQUE NOT NULL,
+            role TEXT NOT NULL,
+            password_hash TEXT NOT NULL
         );
 EOF
 }
