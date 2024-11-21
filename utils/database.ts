2ac8368cd0b933a9cffa2de39244bf08b044f0d4
charnould <charnould@icloud.com>
2024-11-21 10:49:51 +0100

    refactor: revamp vector search to support knowledge access permissions ( + add TS types)

--- utils/database.ts
+++ utils/database.ts
@@ -1,13 +1,32 @@
 import { Database } from 'bun:sqlite'
 import * as sqlite_vec from 'sqlite-vec'
+import { z } from 'zod'
+
 // Builtin SQLite library on MacOS doesn't allow extensions
 Database.setCustomSQLite('/opt/homebrew/opt/sqlite/lib/libsqlite3.dylib')
 
-export const db = (db_name: 'knowledge' | 'telemetry'): sqlite_vec.Db | Database => {
-  if (db_name === 'knowledge') {
-    const knowledge_db: sqlite_vec.Db = new Database('./utils/knowledge/datastore.sqlite')
-    sqlite_vec.load(knowledge_db)
-    return knowledge_db
+// Database Factory Function
+//
+// This function initializes and returns a database connection based on the specified database name.
+// It supports multiple predefined database names, each pointing to a specific SQLite file.
+// For vector databases, the `sqlite_vec` module is loaded to provide vector search.
+export const db = (db_name: Db_Name): Database | (Database & sqlite_vec.Db) => {
+  if (db_name === 'community') {
+    const db = new Database('./knowledge/.data.sqlite')
+    sqlite_vec.load(db)
+    return db
+  }
+
+  if (db_name === 'proprietary.private') {
+    const db = new Database('./knowledge/proprietary/.data.private.sqlite')
+    sqlite_vec.load(db)
+    return db
+  }
+
+  if (db_name === 'proprietary.public') {
+    const db = new Database('./knowledge/proprietary/.data.public.sqlite')
+    sqlite_vec.load(db)
+    return db
   }
 
   if (db_name === 'telemetry') {
@@ -17,3 +36,7 @@ export const db = (db_name: 'knowledge' | 'telemetry'): sqlite_vec.Db | Database
 
   throw new Error('Invalid db_name. Database not initialized.')
 }
+
+// Zod schema/TS type
+const Db_Name = z.enum(['community', 'proprietary.private', 'proprietary.public', 'telemetry'])
+export type Db_Name = z.infer<typeof Db_Name>
