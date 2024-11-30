9333e9099fc8189bbb6920942b782550aefda858
charnould <charnould@icloud.com>
2024-11-30 17:23:48 +0100

    perf: refactor AI agents’ routing for improved response efficiency + minor adjustments to prompts

--- utils/generate-answer.ts
+++ utils/generate-answer.ts
@@ -6,10 +6,11 @@ import { generate_answer, stream_answer } from './deliver-answer'
 //
 //
 //
-// Answer with intelligence
 //
 //
-//
+// Provide an intelligent response tailored to users with
+// access to `community` and `proprietary.public` knowledge.
+// Users may include tenants, candidates, or collaborators.
 //
 //
 
@@ -18,10 +19,12 @@ export const answer_user = async (context: AIContext, options: { is_sms: boolean
   context.conversation.push({
     role: 'user',
     content: `
-    
+
+The current date and time is ${new Date().toLocaleString('fr-FR')}.
+
 # YOUR PERSONA
 
-${context.config.context[`${context.current_context}`].persona}.
+${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].persona : ''}.
 
 # OBJECTIVE
 
@@ -70,13 +73,17 @@ Maintain clarity and simplicity in language, ensuring accessibility for all audi
 
 # YOUR AUDIENCE
 
-- ${context.config.context[`${context.current_context}`].audience}.
+- ${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].audience : ''}.
 - ${context.query?.about_user}.
-- user's location: ${context.query?.location.city !== null ? `${context.query?.location.city} (${context.query?.location.zipcode}),` : ''} ${context.query?.location.department !== null ? `${context.query?.location.department}, ` : ''} ${context.query?.location.region !== null ? `${context.query?.location.region}.` : ''}
+- user's location: 
+  ${context.query?.location.city !== null ? `- ${context.query?.location.city} (${context.query?.location.zipcode}),` : ''}
+  ${context.query?.location.department !== null ? `- ${context.query?.location.department}, ` : ''}
+  ${context.query?.location.region !== null ? `- ${context.query?.location.region}.` : ''}
 
 # CONTEXT
 
-${context.chunks}
+${context.chunks.community}
+${context.chunks.public}
 
 ---
 
@@ -85,8 +92,8 @@ Question: ${context.content}.
 Your answer in "${context.query?.lang}" (ISO 639-1 format):`.trim() // Some LLMs don't allow trailing white space (e.g. Anthropic)
   })
 
-  if (options.is_sms === false) return stream_answer(context)
   if (options.is_sms === true) return generate_answer(context)
+  return stream_answer(context)
 }
 
 //
@@ -94,61 +101,170 @@ Your answer in "${context.query?.lang}" (ISO 639-1 format):`.trim() // Some LLMs
 //
 //
 //
-// Answer with deadlock
 //
 //
+// Provide an intelligent response tailored to collaborators with access
+// to `community`, `proprietary.public` and `proprietary.private` knowledge.
+// Users must be collaborators.
 //
 //
-//
 
-export const reach_deadlock = async (context: AIContext, options: { is_sms: boolean }) => {
-  // Start with a prompt containing only chatbot persona
-  let prompt = `# YOUR PERSONA\n\n ${context.config.context[`${context.current_context}`].persona}.`
+export const answer_collaborator = async (context: AIContext, options: { is_sms: boolean }) => {
+  // Add prompt to conversation history
+  context.conversation.push({
+    role: 'user',
+    content: `
 
-  // If context DOES contain profanity
-  if (context.query?.contains_profanity) {
-    prompt += `
+The current date and time is ${new Date().toLocaleString('fr-FR')}.
 
-# INSTRUCTIONS
+Your responses will be guided by the following context:
 
-- You are a polite and respectful AI assistant.
-- Respond with a calm, friendly reminder that you are here to help but will only respond to polite, respectful conversation.
-- Encourage the user to rephrase their question courteously, as mutual respect is key to effective communication.
-- Always respond in a positive, understanding tone that keeps the conversation constructive.
-- Omit emoticons, emojis, or informal punctuation.
-- Explain you are here to help according to your [persona].
+# Persona & Audience Context
 
----
+- **Persona**: ${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].persona : ''}.
+- **Audience**: ${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].audience : ''}.
 
-Your answer in "${context.query.lang}" (ISO 639-1 format):`
-  }
+# Knowledge Sources
 
-  // If context DOES NOT contain profanity
-  else {
-    prompt += `
-    
-# INSTRUCTIONS
+You have access to three distinct knowledge sources, each with its own priority level:
+1. **Collaborators Knowledge**: Contains specific instructions, procedures, data and guidelines critical to housing-related scenarios. This is your highest priority source when answering questions.
+2. **Public Knowledge**: General information related to housing available to the public.
+3. **Community Knowledge**: Data and insights shared by the community of social housing professionals, which may provide useful context but is secondary to the other two sources.  
 
-- You are a courteous and focused assistant.
-- Always use context from previous messages.
-- Avoid repetitive disclaimers.
-- Language-specific considerations: formal "vous" in French.
+# Knowledge Hierarchy
 
-- Respond briefly in one or two sentences.
-- However if a question is about yourself or your capabilities, respond openly and in detail as appropriate.
-- Then follow up by:
-  - Reminding user what kind of question you can ONLY answer.
-  - Encouraging user to ask questions where you can be most effective.
+When answering questions, follow this knowledge priority order:
+1. Primary Source: Collaborators Knowledge (if relevant information is available).
+2. Secondary Source: Public Knowledge (if collaborators knowledge lacks sufficient information).
+3. Tertiary Source: Community Knowledge (if both collaborators and public knowledge don’t provide the needed details).
 
----
+# Response Guidelines
 
-Question: ${context.content}.
+1. **Understand the User’s Question**: Carefully analyze the user’s request to determine their needs.
+2. **Provide a Direct Answer**: Offer a precise and clear response based on the most relevant knowledge available.
+3. **Rephrase Relevant Knowledge**: If necessary, reword and format the full relevant knowledge chunks that apply to the query, prioritizing clarity and actionable insights.
+4. **Format Your Response**: Structure your response using clear headings, bullet points, and bold text for emphasis. Avoid the use of smileys, emojis, or informal language.
 
-Your answer in "${context.query?.lang}" (ISO 639-1 format):`
-  }
+# Knowledge Chunks
 
-  context.conversation.push({ role: 'user', content: prompt.trim() })
+## Collaborators Knowledge
 
-  if (options.is_sms === false) return stream_answer(context)
+<collaborators_knowledge>
+${context.chunks.private}
+</collaborators_knowledge>
+
+## Public Knowledge
+
+<public_knowledge>
+${context.chunks.public}
+</public_knowledge>
+
+##Community Knowledge
+
+<community_knowledge>
+${context.chunks.community}
+</community_knowledge>
+
+If any of these knowledge types are irrelevant or empty, disregard them in your response.
+
+`.trim() // Some LLMs don't allow trailing white space (e.g. Anthropic)
+  })
+
   if (options.is_sms === true) return generate_answer(context)
+  return stream_answer(context)
+}
+
+//
+//
+//
+//
+//
+//
+//
+// Generate a response indicating that PIERRE cannot provide
+// an answer due to a deadlock. This occurs when the reranker
+// returns no relevant results, meaning PIERRE lacks the
+// necessary knowledge.
+//
+//
+
+export const reach_relevancy_deadlock = async (
+  context: AIContext,
+  options: { is_sms: boolean }
+) => {
+  context.conversation.push({
+    role: 'user',
+    content: `
+  
+The current date and time is ${new Date().toLocaleString('fr-FR')}.
+
+Your responses will be guided by the following context:
+
+# Your Persona
+
+${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].persona : ''}
+
+# Response Guidelines
+
+- **Concise Responses**: For clear and well-established topics, provide a concise response of up to two short sentences, ensuring that the information is precise and unambiguous. But explain also that this subject os outside the scope of your current knowledge base (you should clearly inform the user of this limitation).
+- **Knowledge Integrity Caution**: If you are uncertain or there is a potential for inaccuracy, acknowledge the possibility of gaps in your knowledge, and express a preference to refrain from answering rather than risk providing incorrect information.
+- **Persona Clarity**: Ensure your responses align with your persona’s characteristics. This helps reinforce your identity and strengthens the user’s trust in your guidance.
+- **Request for Clarification**: Encourage the user to reformulate or clarify their query to make it more specific to housing-related concerns or to increase the clarity of the request, making it easier to provide a relevant answer.
+
+---
+
+Your answer in "${context.query?.lang}" (ISO 639-1 format):
+`.trim()
+  })
+
+  if (options.is_sms === true) return generate_answer(context)
+  return stream_answer(context)
+}
+
+//
+//
+//
+//
+//
+//
+//
+// Craft a response explaining that PIERRE cannot provide
+// an answer due to a profanity deadlock (e.g., inappropriate
+// content, profanity).
+//
+//
+
+export const reach_profanity_deadlock = async (
+  context: AIContext,
+  options: { is_sms: boolean }
+) => {
+  context.conversation.push({
+    role: 'user',
+    content: `
+  
+The current date and time is ${new Date().toLocaleString('fr-FR')}.
+
+Your responses will be guided by the following context:
+
+# Persona
+
+${typeof context.config !== 'string' ? context.config.context[`${context.current_context}`].persona : ''}
+
+# Response Guidelines
+
+- You are a calm, polite, and professional AI assistant, dedicated to supporting users with respect and courtesy.
+- When encountering inappropriate language or behavior, kindly remind the user that you are here to assist them, but only engage in respectful and constructive conversations.
+- Encourage users to rephrase their questions or comments in a more polite and respectful manner. Remind them that mutual respect is the foundation of effective communication.
+- Always respond in a positive and understanding tone, ensuring that your responses maintain professionalism and empathy.
+- Avoid using emoticons, emojis, or casual punctuation, as your communication should be formal and respectful.
+- Stay consistent with your persona, ensuring that your responses align with your goal of offering professional assistance while fostering a respectful atmosphere.
+
+---
+
+Your answer in "${context.query?.lang}" (ISO 639-1 format):
+`.trim()
+  })
+
+  if (options.is_sms === true) return generate_answer(context)
+  return stream_answer(context)
 }
