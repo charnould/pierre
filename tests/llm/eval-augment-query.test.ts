e8b6f4af24b097732b5e7180068f941edfbbc3f1
charnould <charnould@icloud.com>
2024-12-08 11:11:37 +0100

    perf: improve `augment-query` prompt

--- tests/llm/eval-augment-query.test.ts
+++ tests/llm/eval-augment-query.test.ts
@@ -4,6 +4,7 @@ import { augment_query } from '../../utils/augment-query'
 // Add `.skip` to make `bun test` pass, otherwise, it'll fail
 // because a missing OPENAI_API_KEY. Remove `.skip` for testing
 // using `bun eval:query`
+
 test.skip('evaluate query augmentation perf.', async () => {
   const context = {
     conv_id: '9efbd713-8e57-444a-94b7-47d76e1755f5',
@@ -13,19 +14,20 @@ test.skip('evaluate query augmentation perf.', async () => {
     },
     role: 'user',
     timestamp: null,
-    conversation: [
-      // { role: 'user', content: "bonjour" },
-      //      { role: 'user', content: 'qui est xavier nicolas ?' }
-      //{ role: 'user', content: 'quell est el chauffage de la résidence Les Pleiades ?' },
-      {
-        role: 'user',
-        content:
-          "Si il y a une absence partielle d'électricité dans les parties communes à 23h dans l'ilot Corse, dois je sortir (je suis agent d'astreinte)"
-      }
 
-      //{ role: 'user', content: "Qui est d'astrein,te en ce moment ?" },
+    // prettier-ignore
+    // biome-ignore format: readability
+    conversation: [
+      { role: 'user', content: 'Quelle est la procédure si un locataire est bloqué dans un ascenseur ?' }
+      // { role: 'user', content: "qui est Obama ?" },
+      // { role: 'user', content: "bonjour" },
+      // { role: 'user', content: 'qui est xavier nicolas ?' }
+      // { role: 'user', content: 'Comment joindre Proxiserve ?' }
+      // { role: 'user', content: 'quell est el chauffage de la résidence Les Pleiades ?' },
+      // { role: 'user', content: "Si il y a une absence partielle d'électricité dans les parties communes à 23h dans l'ilot Corse, dois je sortir (je suis agent d'astreinte)" }
+      // { role: 'user', content: "Qui est d'astrein,te en ce moment ?" },
       // { role: 'user', content: "qui est d'astreinte en ce moment" },
-      //  { role: 'user', content: 'qui al es clés des portes anti-squat ?' },
+      // { role: 'user', content: 'qui al es clés des portes anti-squat ?' },
       // { role: 'user', content: "Bonjour, mon voisin fait du bruit tous les soirs, que puis-je faire et il en tord ? j'hahbite à piolenc et orange" },
       // { role: 'user', content: 'comment déposer une demande de logement social' },
       // { role: 'assistant', content: 'Rendez-vous sur le site dédié' },
