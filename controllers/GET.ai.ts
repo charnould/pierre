0ff44cd8307891ec7b49f80ed29b22482232fc46
charnould <charnould@icloud.com>
2025-01-12 15:37:54 +0100

    fix: add server error handling + add perf. measurements + improve comments
    Former-commit-id: 7d911fc0d5704305f0e7905f83c1d18f7cb9968d

--- controllers/GET.ai.ts
+++ controllers/GET.ai.ts
@@ -15,11 +15,12 @@ import { rank_chunks } from '../utils/rank-chunks'
 import { bm25_search } from '../utils/search-by-bm25'
 import { vector_search } from '../utils/search-by-vectors'
 import { send_sms } from '../utils/send-sms'
+import { view } from '../views/llm-error'
 
 export const controller = async (c: Context) => {
   try {
-    console.time('full_request')
-    console.time('augment_query')
+    // Mark the start of the request for performance measurement
+    performance.mark('s_rq')
 
     //
     // Step 1.
@@ -29,7 +30,7 @@ export const controller = async (c: Context) => {
     let is_sms = false
     let parsed_sms: SMS = null
     let context: AIContext
-    let answer: string | StreamTextResult<Record<string, CoreTool>>
+    let answer: string | StreamTextResult<Record<string, CoreTool>, unknown>
 
     //
     // Step 2.
@@ -65,32 +66,28 @@ export const controller = async (c: Context) => {
     // The Main Part: "Apply AI Logic"
     //
 
-    // Save reply (= what the user just said) and because
-    // of `true` save also this reply to PIERRE (= telemetry)
-    // if `telemetry` env. variable is also `true`
+    // Save reply (= what the user just said) and because of `true` save also this
+    // reply to PIERRE (= telemetry) if `telemetry` env. variable is also `true`
     save_reply(context, true)
 
-    // Add up conversation context with full conversation
-    // (= everything user/AI have said) and augment+enrich
-    // last user input
+    // Mark the start of the "augment_query" operation for performance measurement
+    performance.mark('s_aq')
+
+    // Add the full conversation to the context
     context = await AIContext.parseAsync({
       ...context,
       conversation: get_conversation(context.conv_id)
-    }).then(async (context) => ({ ...context, query: await augment_query(context) }))
+    })
 
-    console.timeEnd('augment_query')
+    // Use the updated context to augment the query
+    context.query = await augment_query(context)
+
+    // Mark the end of the "augment_query" operation for performance measurement
+    performance.mark('e_aq')
 
     // If query is relevant and does not contain profanity,
     // answer with intelligence
     if (context.query && !context.query.contains_profanity) {
-      //
-      //
-      //
-      console.debug('🤖 should answer with intelligence')
-      //
-      //
-      //
-
       // Get knowledge access for user query considering current context
       let knowledge_access = { community: true, proprietary: { public: false, private: false } }
       if (typeof context.config !== 'string') {
@@ -102,7 +99,8 @@ export const controller = async (c: Context) => {
       // If `context.chunks` contains bullshit, PIERRE
       // answer will probably be bullshit!
 
-      console.time('vector_search')
+      // Mark the start of vector search operation for performance measurement
+      performance.mark('s_vs')
 
       const v_results = await Promise.all(
         [
@@ -114,66 +112,51 @@ export const controller = async (c: Context) => {
         ].map((q) => vector_search(q, context))
       )
 
-      console.timeEnd('vector_search')
-      console.time('bm25_search')
+      // Mark the end of vector search operation for performance measurement
+      performance.mark('e_vs')
+
+      // Mark the start of BM25 search operation for performance measurement
+      performance.mark('s_bs')
 
       const k_results = context.query.bm25_keywords.map((k) => bm25_search(k, context))
 
-      console.timeEnd('bm25_search')
-      console.time('reranker')
+      // Mark the end of BM25 search operation for performance measurement
+      performance.mark('e_bs')
 
+      // Mark the start of reranker operation for performance measurement
+      performance.mark('s_rr')
       context.chunks = await rank_chunks(
         v_results.filter((chunk) => chunk !== undefined), // TODO: vector_search must not return undefined
         k_results.filter((chunk) => chunk !== undefined), // TODO: vector_search must not return undefined
         context
       )
 
-      console.timeEnd('reranker')
-
-      //
-      //
-      //
-      console.debug('-----------------------------------------')
-      console.debug('🤖 augments the user query to:')
-      console.debug(context)
-      console.debug('-----------------------------------------')
-      //
-      //
-      //
+      // Mark the end of reranker operation for performance measurement
+      performance.mark('e_rr')
 
       // If the reranker returns no relevant results, it indicates that
       // either the question is unrelated to housing  or PIERRE lacks
       // the necessary knowledge to address it. In both cases, respond
       // with a deadlock.
       if (
-        context.chunks.community.length === 0 &&
-        context.chunks.private.length === 0 &&
-        context.chunks.public.length === 0
+        (context.chunks.community?.length ?? 0) === 0 &&
+        (context.chunks.private?.length ?? 0) === 0 &&
+        (context.chunks.public?.length ?? 0) === 0
       ) {
-        console.debug('🤖 responding with a no-knowledge deadlock.')
+        console.debug('No knowledge chunk: respond with a no-knowledge deadlock.')
         answer = await reach_relevancy_deadlock(context, { is_sms: is_sms })
       }
       // If private knowledge access is `true` (e.g. internal process),
-      // it means that user MUST be a collaborator. Hence, answer
-      // must be specific to this use case.
+      // it means that user MUST be a collaborator. Hence, answer must
+      // be specific to this use case.
       else if (knowledge_access.proprietary.private === true) {
-        console.debug('🤖 responding to a collaborator.')
         answer = await answer_collaborator(context, { is_sms: is_sms })
       } else {
         // If private knowledge access is `false`,
         // answer user like a normal user
-        console.debug('🤖 responding to a normal user.')
         answer = await answer_user(context, { is_sms: is_sms })
       }
     } else {
-      //
-      //
-      //
-      console.debug('🤖 responding with a profanity deadlock')
-      //
-      //
-      //
-
       // If query is irrelevant and/or contains profanity,
       // make a deadlock answer
       answer = await reach_profanity_deadlock(context, { is_sms: is_sms })
@@ -184,8 +167,7 @@ export const controller = async (c: Context) => {
     // Send answer to the user
     //
 
-    // If incoming request is a SMS,
-    // return a full-text answer
+    // If incoming request is a SMS: return a full-text answer
     if (is_sms && parsed_sms !== null) {
       await send_sms({
         from: (context.config as Config).phone,
@@ -196,18 +178,25 @@ export const controller = async (c: Context) => {
       return c.body('ok', 200)
     }
 
-    console.timeEnd('full_request')
+    // Mark the end of the request for performance measurement
+    // and log a performance measurement table
+    performance.mark('e_rq')
+    console.table([
+      ['Augment_q', `${performance.measure('r', 's_aq', 'e_aq').duration}ms`],
+      ['Vector_s ', `${performance.measure('r', 's_vs', 'e_vs').duration}ms`],
+      ['bm25_s   ', `${performance.measure('r', 's_bs', 'e_bs').duration}ms`],
+      ['reranker ', `${performance.measure('r', 's_rr', 'e_rr').duration}ms`],
+      ['TOTAL    ', `${performance.measure('r', 's_rq', 'e_rq').duration}ms`]
+    ])
 
-    // If incoming request comes from
-    // the web, return a text stream.
-
-    // c.header('Content-Type', 'text/plain; charset=utf-8')
+    // If incoming request comes from www: return a stream
     c.header('Content-Type', 'text/event-stream')
 
     return stream(c, (stream) =>
-      stream.pipe((answer as StreamTextResult<Record<string, CoreTool>>).textStream)
+      stream.pipe((answer as StreamTextResult<Record<string, CoreTool>, unknown>).textStream)
     )
   } catch (e) {
     console.error(e)
+    return c.html(view())
   }
 }
