6261787ba52e0ef7f03de0f744590faa3f1c7d5c
charnould <charnould@icloud.com>
2025-01-17 11:28:43 +0100

    fix: fetch lastest `pierre` version from GitHub API
    Former-commit-id: a915c4d0576a490dce385348d7840bf79173b02a

--- utils/update-pierre.ts
+++ utils/update-pierre.ts
@@ -1,9 +1,20 @@
 import { $ } from 'bun'
 import chalk from 'chalk'
 
-const latest_version = (
-  await $`git fetch --tags && git describe --tags $(git rev-list --tags --max-count=1)`.text()
-).trim()
+const url = 'https://api.github.com/repos/charnould/pierre/releases'
+let latest_version: string | undefined
+
+try {
+  const response = await fetch(url)
+  if (!response.ok) throw new Error(`Failed to fetch ${url}`)
+  const releases: { tag_name: string }[] = await response.json()
+
+  if (releases.length > 0) latest_version = releases[0].tag_name
+  else latest_version = undefined
+} catch (error) {
+  console.error('Error fetching the latest version:', error)
+  latest_version = undefined
+}
 
 const current_version = (
   await $`git describe --tags $(git rev-list --tags --max-count=1)`.text()
@@ -30,6 +41,9 @@ if (latest_version === current_version) {
   } catch (e) {
     // console.log(chalk.red('`config.ts` contient des erreurs.'))
   }
+} else if (latest_version === undefined || current_version === undefined) {
+  console.log(chalk.red('Une anomalie est intervenue'))
+  console.log('')
 } else {
   console.log(chalk.red.bold("PIERRE n'est pas à jour."))
   console.log(chalk.red.underline('https://github.com/charnould/pierre/releases'))
