363bbbba06a046d774883a4152badae02bef3022
charnould <charnould@icloud.com>
2025-01-17 11:09:31 +0100

    refactor: add import statement for `cerebras`, `groq` and `together` + make prompts clearer

--- utils/deliver-answer.ts
+++ utils/deliver-answer.ts
@@ -1,8 +1,11 @@
 import { createAnthropic } from '@ai-sdk/anthropic'
+import { createCerebras } from '@ai-sdk/cerebras'
 import { createCohere } from '@ai-sdk/cohere'
 import { createGoogleGenerativeAI } from '@ai-sdk/google'
+import { createGroq } from '@ai-sdk/groq'
 import { createMistral } from '@ai-sdk/mistral'
 import { createOpenAI } from '@ai-sdk/openai'
+import { createTogetherAI } from '@ai-sdk/togetherai'
 import { generateText, smoothStream, streamText } from 'ai'
 import remove_markdown from 'remove-markdown'
 import type { AIContext } from './_schema'
@@ -13,6 +16,9 @@ const google = createGoogleGenerativeAI()
 const anthropic = createAnthropic()
 const mistral = createMistral()
 const cohere = createCohere()
+const togetherai = createTogetherAI()
+const groq = createGroq()
+const cerebras = createCerebras()
 
 //
 // If incoming conversation comes from the World Wide Web,
