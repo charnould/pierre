2d75dbe3755e460c51b6cf62edea373a30af1eee
charnould <charnould@icloud.com>
2025-02-01 17:44:41 +0100

    test: update query augmentation models in evaluation tests
    Former-commit-id: 6f6ef1c956d02d55ff42d23052c6836a1f7fe34d

--- tests/llm/eval-augment-query.test.ts
+++ tests/llm/eval-augment-query.test.ts
@@ -11,7 +11,7 @@ test('evaluate query augmentation perf.', async () => {
           models: {
             embed_with: 'text-embedding-3-large',
             // Change `augment_with` to test different models
-            augment_with: "openai('gpt-4o-mini-2024-07-18')",
+            augment_with: "togetherai('meta-llama/Llama-3.3-70B-Instruct-Turbo')", // "openai('gpt-4o-mini-2024-07-18')",
             rerank_with: "openai('gpt-4o-mini-2024-07-18')",
             answer_with: "openai('gpt-4o-mini-2024-07-18')"
           }
@@ -21,26 +21,25 @@ test('evaluate query augmentation perf.', async () => {
     current_context: 'default',
     conversation: [
       // { role: 'user', content: 'Tu es trop con !' },
-      // { role: 'user', content: 'Comment déposer mon préavis de congé pour mon logement ? Et avez-vous un modèle de courrier ?' }
       // { role: 'user', content: 'Comment déposer mon préavis de congé pour mon logement ? Et avez-vous un modèle de courrier ?' },
       // { role: 'user', content: 'كيفية الاتصال بالمكتب الرئيسي لبلدية Grand Dijon Habitat؟' }
       // { role: 'user', content: 'Quelle est la procédure si un locataire est bloqué dans un ascenseur ?' }
-      { role: 'user', content: 'qui est Obama ?' }
+      // { role: 'user', content: 'qui est Obama ?' }
       // { role: 'user', content: "bonjour" },
       // { role: 'user', content: " كيفية الاتصال بالمكتب الرئيسي لبلدية Grand Dijon Habitat؟" },
       // { role: 'user', content: 'qui est xavier nicolas ?' }
       // { role: 'user', content: 'Comment joindre Proxiserve ?' }
       // { role: 'user', content: 'quell est el chauffage de la résidence Les Pleiades ?' },
       // { role: 'user', content: "Si il y a une absence partielle d'électricité dans les parties communes à 23h dans l'ilot Corse, dois je sortir (je suis agent d'astreinte)" }
-      // { role: 'user', content: "Qui est d'astrein,te en ce moment ?" },
-      // { role: 'user', content: "qui est d'astreinte en ce moment" }
+      { role: 'user', content: "qui est d'astreinte aujourd'hui " }
       // { role: 'user', content: 'qui al es clés des portes anti-squat ?' },
       // { role: 'user', content: "Bonjour, mon voisin fait du bruit tous les soirs, que puis-je faire et il en tord ? j'hahbite à piolenc et orange" }
       // { role: 'user', content: 'comment déposer une demande de logement social' },
+      // { role: 'user', content: 'qui es tu ? C koi un logement social, et combien y en a en france ?' },
       // { role: 'assistant', content: 'Rendez-vous sur le site dédié' },
-      // { role: 'user', content: "c'est pour un T3 près de Paris" },
+      // { role: 'user', content: "je cherche un logement, c'est pour un T3 près de Paris" },
       // { role: 'assistant', content: "entendu, je réfléchis" },
-      // { role: 'user', content: "si c'est important, je suis célibataire avec 2 enfants à charge" }
+      // { role: 'user', content: "c'est important, je suis célibataire avec 2 enfants à charge" }
     ]
   }
 
