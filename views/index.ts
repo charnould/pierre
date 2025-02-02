51004378eabfccdf082bd560517ffbe7f74e58b4
charnould <charnould@icloud.com>
2025-02-02 14:12:21 +0100

    build: rebuild frontend assets
    Former-commit-id: 81e2f68e7352987737465df97cb53faa416f361d

--- views/index.ts
+++ views/index.ts
@@ -9,8 +9,8 @@ export const view = (config: Config, context: string) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="../assets/${config.id}/system.svg" type="image/svg+xml" />
         <link rel="manifest" href="../assets/${config.id}/manifest.json" />
-        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738245051611.css" />
-        <script type="module" src="../assets/pierre-ia.org/dist/js/ai.1738245051611.js"></script>
+        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738501854356.css" />
+        <script type="module" src="../assets/pierre-ia.org/dist/js/ai.1738501854356.js"></script>
         <title>Comment puis-je vous aider ? 🖐️</title>
       </head>
 
--- views/login.ts
+++ views/login.ts
@@ -8,7 +8,7 @@ export const view = (message) => {
         <script src="https://unpkg.com/@hotwired/turbo@8.0.12/dist/turbo.es2017-umd.js"></script>
         <link rel="icon" href="../assets/pierre-ia.org/system.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738245051611.css" />
+        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738501854356.css" />
         <title>PIERRE — Administration > Connexion</title>
       </head>
 
