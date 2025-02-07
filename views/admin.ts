b70295c82965e770f5fa54cc59fb1a2aef17a4f4
charnould <charnould@icloud.com>
2025-02-07 11:57:54 +0100

    build: (re)add Turbo to admin, conversations, login, and users views
    Former-commit-id: 00052e68af4bf49dbab60c4924c90ac361894faa

--- views/admin.ts
+++ views/admin.ts
@@ -5,6 +5,7 @@ export const view = (user) => {
     <html lang="fr">
       <head>
         <meta charset="UTF-8" />
+        <script src="https://unpkg.com/@hotwired/turbo@8.0.12/dist/turbo.es2017-umd.js"></script>
         <link rel="icon" href="../assets/pierre-ia.org/system.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738923107833.css" />
