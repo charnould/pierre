68dac4f534b100d621d20d2e42f327421ef83abf
charnould <charnould@icloud.com>
2025-02-07 11:57:31 +0100

    refactor: optimize performance view by generating plots server-side
    Former-commit-id: 6371ead3bf615eab7196df4005b69dd3e6aa3a73

--- views/performance.ts
+++ views/performance.ts
@@ -1,54 +1,74 @@
+import * as Plot from '@observablehq/plot'
 import { html, raw } from 'hono/html'
+import { JSDOM } from 'jsdom'
 import type { StatisticOptions } from '../controllers/GET.performance'
+
+/**
+ * Generates a graphic plot based on the provided data and options.
+ *
+ * @param data - The data to be plotted, in JSON string format.
+ * @param options - An object containing various options for the statistic plot.
+ * @returns The outer HTML of the generated plot as a string.
+ *
+ * @typedef {Object} StatisticOptions
+ * @property {string} window - The window option for the plot.
+ * @property {string} color - The color option for the plot.
+ * @property {string} facet - The facet option for the plot.
+ */
+const generate_graphic = (data, options: StatisticOptions) => {
+  const plot = Plot.waffleY(
+    JSON.parse(data),
+    Plot.groupX(
+      { y: 'count' },
+      {
+        x: `${options.window}`,
+        fill: `${options.color}`,
+        sort: `${options.color}`,
+        fy: `${options.facet}`
+      }
+    )
+  ).plot({
+    document: new JSDOM('').window.document,
+    marks: [
+      Plot.ruleY([0]),
+      Plot.axisY({ anchor: 'left', label: 'Nombre de conversations', labelAnchor: 'top' }),
+      Plot.axisX({ tickRotate: -35 })
+    ],
+    width: 1024,
+    marginBottom: 100,
+    x: {
+      label: null,
+      type: 'band'
+    },
+    color: {
+      legend: true,
+      domain: ['0', '1', '2', '3', 'Non noté'],
+      range: ['#fb2c36', '#ffb900', '#9ae600', '#00c951', '#44403b']
+    }
+  })
+
+  plot.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/2000/svg')
+  plot.setAttributeNS(
+    'http://www.w3.org/2000/xmlns/',
+    'xmlns:xlink',
+    'http://www.w3.org/1999/xlink'
+  )
+
+  return plot.outerHTML
+}
 
 export const view = (data, options: StatisticOptions) => {
   return html`<!doctype html>
     <html lang="fr">
       <head>
         <meta charset="UTF-8" />
+        <script src="https://unpkg.com/@hotwired/turbo@8.0.12/dist/turbo.es2017-umd.js"></script>
         <link rel="icon" href="../assets/pierre-ia.org/system.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1738923107833.css" />
         <title>PIERRE — Administration > Performance</title>
       </head>
 
-      <script type="module">
-        import * as Plot from 'https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm'
-        const data = ${raw(data)}
-
-        const plot = Plot.waffleY(
-          data,
-          Plot.groupX(
-            { y: 'count' },
-            {
-              x: '${raw(options.window)}',
-              fill: '${raw(options.color)}',
-              sort: '${raw(options.color)}',
-              fy: '${raw(options.facet)}'
-            }
-          )
-        ).plot({
-          marks: [
-            Plot.ruleY([0]),
-            Plot.axisY({ anchor: 'left', label: 'Nombre de conversations', labelAnchor: 'top' }),
-            Plot.axisX({ tickRotate: -35 })
-          ],
-          width: 1024,
-          marginBottom: 100,
-          x: {
-            label: null,
-            type: 'band'
-          },
-          color: {
-            legend: true,
-            domain: ['0', '1', '2', '3', 'Non noté'],
-            range: ['#fb2c36', '#ffb900', '#9ae600', '#00c951', '#44403b']
-          }
-        })
-
-        const div = document.querySelector('#myplot')
-        div.append(plot)
-      </script>
       <body class="m-auto mt-10 mb-0 w-6xl min-w-6xl bg-stone-50">
         <a href="/a" class="mb-4 block w-fit rounded-full bg-teal-200 px-3 py-1 pr-4 font-semibold"
           >← Retour</a
@@ -64,6 +84,7 @@ export const view = (data, options: StatisticOptions) => {
           >
             <h1 class="text-6xl font-bold">Performance</h1>
             <button
+              data-turbo="false"
               type="submit"
               name="action"
               value="download"
@@ -134,7 +155,7 @@ export const view = (data, options: StatisticOptions) => {
             />
           </form>
 
-          <div id="myplot" class="m-auto"></div>
+          <div class="m-auto">${raw(generate_graphic(data, options))}</div>
         </div>
       </body>
     </html>`
