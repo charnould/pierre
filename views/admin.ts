0d6f76d012426fb46fc9f3c973e79c3b89060168
charnould <charnould@icloud.com>
2025-02-12 15:00:52 +0100

    minor redesign for improved user experience

--- views/admin.ts
+++ views/admin.ts
@@ -8,34 +8,51 @@ export const view = (user) => {
         <script src="https://unpkg.com/@hotwired/turbo@8.0.12/dist/turbo.es2017-umd.js"></script>
         <link rel="icon" href="../assets/pierre-ia.org/system.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1739174979163.css" />
-        <title>PIERRE — Administration</title>
+        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1739741252036.css" />
+        <title>PIERRE — Console</title>
       </head>
 
-      <body class="w-[900px] p-20">
+      <body class="mt-20 ml-20 w-4xl min-w-4xl bg-stone-50 font-mono">
+        <p class="mb-6 block w-fit rounded-full bg-teal-200 px-4 py-2 pr-4 text-xl font-medium">
+          PIERRE-IA.org
+        </p>
+
         <a
           href="a/conversations"
           ${user.role === 'administrator'
-            ? html`class="block cursor-pointer text-7xl font-bold hover:underline
+            ? html`class="block cursor-pointer text-7xl font-extrabold mb-1 hover:underline
               hover:underline-offset-4"`
-            : html`class="block cursor-not-allowed text-7xl font-bold text-neutral-400"`}
+            : html`class="block cursor-pointer text-7xl font-extrabold mb-1 cursor-not-allowed
+              text-neutral-400"`}
           >Conversations</a
+        >
+
+        <a
+          href="a/knowledge"
+          ${user.role === 'administrator'
+            ? html`class="block cursor-pointer text-7xl font-extrabold mb-1 hover:underline
+              hover:underline-offset-4"`
+            : html`class="block cursor-pointer text-7xl font-extrabold mb-1 cursor-not-allowed
+              text-neutral-400"`}
+          >Encyclopédie</a
         >
 
         <a
           href="a/performance"
           ${user.role === 'administrator'
-            ? html`class="block cursor-pointer text-7xl font-bold hover:underline
+            ? html`class="block cursor-pointer text-7xl font-extrabold mb-1 hover:underline
               hover:underline-offset-4"`
-            : html`class="block cursor-not-allowed text-7xl font-bold text-neutral-400"`}
+            : html`class="block cursor-pointer text-7xl font-extrabold mb-1 cursor-not-allowed
+              text-neutral-400"`}
           >Performance</a
         >
         <a
           href="a/users"
           ${user.role === 'administrator'
-            ? html`class="block cursor-pointer text-7xl font-bold hover:underline
+            ? html`class="block cursor-pointer text-7xl font-extrabold mb-1 hover:underline
               hover:underline-offset-4"`
-            : html`class="block cursor-not-allowed text-7xl font-bold text-neutral-400"`}
+            : html`class="block cursor-pointer text-7xl font-extrabold mb-1 cursor-not-allowed
+              text-neutral-400"`}
           >Utilisateurs</a
         >
 
