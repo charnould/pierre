2ca09eb6db0a587a5b193a21caa68ee85d96a936
charnould <charnould@icloud.com>
2025-02-13 10:46:16 +0100

    fix: truncate hash output to 20 characters and update documentation/test
    Former-commit-id: d8a4b6e0db7ae99872dfe7b8e4908cfb20bf8b9a

--- tests/unit/utils/generate-hash.test.ts
+++ tests/unit/utils/generate-hash.test.ts
@@ -3,7 +3,7 @@ import { generate_hash } from '../../../utils/knowledge/generate-hash'
 
 it('should hash a string', async () => {
   const to_hash = 'Bonjour'
-  const hashed = 'kXLo7smfFE9y7KmlaHWVgO2tss_RVIV_BZXVpSTvEQ'
+  const hashed = 'kXLo7smfFE9y7KmlaHWV'
 
   expect(generate_hash(to_hash)).toStrictEqual(hashed)
 })
