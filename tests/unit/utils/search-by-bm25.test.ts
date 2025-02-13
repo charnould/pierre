00373de35c4654c7bc5a07182703d30dc5170a2b
charnould <charnould@icloud.com>
2025-02-13 10:47:41 +0100

    refactor: remove entity_hash from database schema and related queries

--- tests/unit/utils/search-by-bm25.test.ts
+++ tests/unit/utils/search-by-bm25.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test'
-import { bm25_search, query_db } from '../../../utils/search-by-bm25'
+import { query_db } from '../../../utils/search-by-bm25'
 
 test('should bm25 search works', async () => {
   const context = (await import('../../../tests/unit/utils/__mocks__/ai_context.json')).default
@@ -8,7 +8,7 @@ test('should bm25 search works', async () => {
   // "problème de voisinage", "règlement de conflit",
   // "conseils voisinage", "médiation", "relations de voisinage"
 
-  const r = query_db('proprietary.private', 'skydome')
+  const r = query_db('community', 'Valérie Létard, née le 13 octobre 1962')
 
   expect(r).toMatchSnapshot()
 })
