0cefc515ccafcda76d4ec05ca8d7b7b07b3edcc0
charnould <charnould@icloud.com>
2025-03-01 21:04:08 +0100

    fix: update stylesheet and script links to latest versions
    Former-commit-id: 5523e48d09cd9682166b68c6d4bbef15a81c8a0c

--- views/index.ts
+++ views/index.ts
@@ -9,8 +9,8 @@ export const view = (config: Config, context: string) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="../assets/${config.id}/system.svg" type="image/svg+xml" />
         <link rel="manifest" href="../assets/${config.id}/manifest.json" />
-        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1740850722381.css" />
-        <script type="module" src="../assets/pierre-ia.org/dist/js/ai.1740850722381.js"></script>
+        <link rel="stylesheet" href="../assets/pierre-ia.org/dist/css/style.1740859292637.css" />
+        <script type="module" src="../assets/pierre-ia.org/dist/js/ai.1740859292637.js"></script>
         <title>Comment puis-je vous aider ? 🖐️</title>
       </head>
 
