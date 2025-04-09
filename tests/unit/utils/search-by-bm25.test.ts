0cf9ba1baa24b2211185dfe37fc70e9b31ce7b0a
Charles-Henri Arnould <charnould@icloud.com>
2025-04-09 11:20:29 +0200

    test: update bm25 test case and snapshot

--- tests/unit/utils/search-by-bm25.test.ts
+++ tests/unit/utils/search-by-bm25.test.ts
@@ -1,13 +1,7 @@
-import { expect, test } from 'bun:test'
+import { expect, it } from 'bun:test'
 import { query_db } from '../../../utils/search-by-bm25'
 
-test('should bm25 search works', async () => {
-  const context = (await import('../../../tests/unit/utils/__mocks__/ai_context.json')).default
-
-  // "problème de voisinage", "règlement de conflit",
-  // "conseils voisinage", "médiation", "relations de voisinage"
-
+it('should search using bm25', async () => {
   const r = query_db('community', 'Valérie Létard, née le 13 octobre 1962')
-
   expect(r).toMatchSnapshot()
 })
