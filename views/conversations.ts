70a16a5089d916e5c0c0a46f06303cf70427de5a
Charles-Henri Arnould <charnould@icloud.com>
2025-04-19 15:24:14 +0200

    chore: rebuild frontend assets

--- views/conversations.ts
+++ views/conversations.ts
@@ -14,7 +14,7 @@ export const view = (data, conversation: Reply[] | []) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <link rel="stylesheet" href="../assets/default/dist/css/style.1744706133754.css" />
+        <link rel="stylesheet" href="../assets/default/dist/css/style.1745562419398.css" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:wght@350&display=swap"
           rel="stylesheet"
