eba2a78a3863c99f318f28fc250f8bf4562a51f5
Charles-Henri Arnould <charnould@icloud.com>
2025-04-29 13:47:37 +0200

    feat: add `user` identification support + miscellaneous

--- controllers/GET.conversations.ts
+++ controllers/GET.conversations.ts
@@ -1,18 +1,22 @@
 import type { Context } from 'hono'
 import _ from 'lodash'
-import { view } from '../views/conversations'
-
-import type { Reply } from '../utils/_schema'
+import { Reply } from '../utils/_schema'
 import { get_conversation, get_conversations } from '../utils/handle-conversation'
+import { view } from '../views/conversations'
 
 export const controller = async (c: Context) => {
   let displayed_conversation: Reply[]
   const id = c.req.query('id') as string
+
   if (id !== null) displayed_conversation = get_conversation(id)
 
   const conversations: Reply[] = get_conversations()
-  const conversations_grouped_by_id = _.map(_.groupBy(conversations, 'conv_id'), (group) =>
-    _.reverse(group)
+  const conversations_grouped_by_id = _.map(
+    _.groupBy(
+      conversations.map((c) => Reply.parse(c)),
+      'conv_id'
+    ),
+    (group) => _.reverse(group)
   )
 
   return c.html(view(conversations_grouped_by_id, displayed_conversation))
