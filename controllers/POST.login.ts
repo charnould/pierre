3a08bdea7eae90be3f9e0837e7b3cebfd65d4d38
Charles-Henri Arnould <charnould@icloud.com>
2025-05-14 11:39:17 +0200

    refactor: change user management functionality

--- controllers/POST.login.ts
+++ controllers/POST.login.ts
@@ -1,27 +1,61 @@
+import { readdir } from 'node:fs/promises'
 import type { Context } from 'hono'
 import { deleteCookie, setSignedCookie } from 'hono/cookie'
 import { User } from '../utils/_schema'
 import { encrypt } from '../utils/authenticate-user'
 import { get_user, save_user } from '../utils/handle-user'
 
+/**
+ * Handles the login and logout actions for the application.
+ *
+ * @param c - The context object containing the request and response.
+ *
+ * ### Login Action
+ * - Parses the request body to extract `email`, `password`, and `action`.
+ * - If the `action` is `login`:
+ *   - Special Case: If the email is `admin@pierre-ia.org`:
+ *     - Verifies the password against the environment variable `AUTH_PASSWORD`.
+ *     - If valid, creates and updates the default administrator user.
+ *     - If invalid, redirects to the login page with a `wrong_root_password` error message.
+ *   - For other emails:
+ *     - Attempts to retrieve the user by email.
+ *     - If the user does not exist, redirects to the login page with an `unknown_user` error message.
+ *     - If the user exists but the password is incorrect, redirects to the login page with a `wrong_password` error message.
+ *     - If the email and password are correct:
+ *       - Adjusts the redirection URL if the user's role is `collaborator`.
+ *       - Sets a signed authentication cookie to maintain the user's session.
+ *       - Redirects to the intended page after successful login.
+ *
+ * ### Logout Action
+ * - If the `action` is `logout`:
+ *   - Deletes the authentication cookie.
+ *   - Redirects to the login page.
+ *
+ * ### Authentication Cookie
+ * - The cookie is signed and encrypted using the `AUTH_SECRET` environment variable.
+ * - The cookie contains the user's email, configuration, and role.
+ * - The cookie has a 1-year expiration time.
+ *
+ * @returns Redirects the user to the appropriate page based on the action and authentication status.
+ */
 export const controller = async (c: Context) => {
-  // Parse request body and initialize variables
+  // Initialize redirection URL (default: '/a' if no redirection is provided)
   let redirection = encodeURIComponent(c.req.query('redirection') ?? '/a')
 
+  // Parse the incoming request body for 'email', 'password', and 'action'
   const { email, password, action }: { email: string; password: string; action: string } =
     await c.req.parseBody()
 
   // Handle `login` action
-  // Check credentials, then redirect accordingly
   if (action === 'login') {
-    // Special case for handling the very first user
-    // Create and save the default admin user if credentials match
-    // Otherwise redirect and display an error message
+    // Special case for handling the creation of the default admin user
+    // If the provided credentials match the hardcoded 'admin@pierre-ia.org',
+    // the first user is created and saved. If the password is incorrect, an error message is shown.
     if (email === 'admin@pierre-ia.org') {
       if (password === Bun.env.AUTH_PASSWORD) {
         const default_user = User.parse({
           role: 'administrator',
-          config: 'default',
+          config: JSON.stringify((await readdir('assets')).sort()),
           email: 'admin@pierre-ia.org',
           password_hash: await Bun.password.hash(password)
         })
@@ -32,29 +66,33 @@ export const controller = async (c: Context) => {
       }
     }
 
-    // If email isn't `admin@pierre-ia.org`,
-    // try to retrieve user by email
-    const user = get_user(email.toLowerCase()) as User
+    // For other emails,
+    // attempt to retrieve the user by email
+    const user = get_user(email)
 
-    // Redirect if the user does not exist
+    // If the user does not exist,
+    // redirect to login page with an 'unknown_user' error message
     if (!user) {
       return c.redirect(`/a/login?message=unknown_user&redirection=${redirection}`)
     }
 
-    // If user exists, verify password
+    // If user exists,
+    // verify the password provided by the user
     const is_verified = await Bun.password.verify(password, user.password_hash)
 
-    // Redirect if password verification fails
+    // If password verification fails,
+    // redirect to login page with a 'wrong_password' error message
     if (user && is_verified === false) {
       return c.redirect(`/a/login?message=wrong_password&redirection=${redirection}`)
     }
 
-    // If both `email` and `password` are correct
+    // If the email and password are correct, check the user’s role
+    // If the role is 'collaborator', adjust the redirection URL
     if (user.role === 'collaborator')
       redirection =
         redirection === encodeURIComponent('/a') ? encodeURIComponent('/c') : redirection
 
-    // set a signed authentication cookie for user session
+    // Set a signed authentication cookie to maintain the user's session
     await setSignedCookie(
       c,
       'pierre-ia',
@@ -72,7 +110,6 @@ export const controller = async (c: Context) => {
   }
 
   // Handle `logout` action
-  // Delete cookies, then redirect to login page
   if (action === 'logout') {
     deleteCookie(c, 'pierre-ia')
     return c.redirect('/a/login')
