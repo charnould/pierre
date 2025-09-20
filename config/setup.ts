1b2b9907c0431d58750d9bef873b9f557daf6008
Charles-Henri Arnould <charnould@icloud.com>
2025-09-20 23:54:20 +0200

    refactor: enhance setup process and database initialization

--- config/setup.ts
+++ config/setup.ts
@@ -1,29 +1,41 @@
 import { Database } from 'bun:sqlite'
 import { $ } from 'bun'
+import * as sqlite_vec from 'sqlite-vec'
 
-if (Bun.env.SERVICE === undefined) Bun.env.SERVICE = 'default'
+// Builtin SQLite library on MacOS doesn't allow extensions
+Database.setCustomSQLite('/opt/homebrew/opt/sqlite/lib/libsqlite3.dylib')
 
-await $`mkdir -p datastores/${Bun.env.SERVICE}`
-await $`mkdir -p datastores/${Bun.env.SERVICE}/files`
-await $`mkdir -p datastores/${Bun.env.SERVICE}/__temp__`
+export const setup = async () => {
+  if (Bun.env.SERVICE === undefined) Bun.env.SERVICE = 'default'
 
-const db = new Database(`datastores/${Bun.env.SERVICE}/datastore.sqlite`)
+  await $`mkdir -p datastores/${Bun.env.SERVICE}`
+  await $`mkdir -p datastores/${Bun.env.SERVICE}/files`
+  await $`mkdir -p datastores/${Bun.env.SERVICE}/temp`
 
-db.exec(`
-    CREATE TABLE IF NOT EXISTS telemetry (
-        conv_id TEXT,
-        config TEXT,
-        role TEXT,
-        timestamp TEXT,
-        content TEXT,
-        metadata TEXT,
+  sqlite_vec.load(new Database('./knowledge/data.sqlite'))
+  sqlite_vec.load(new Database(`./datastores/${Bun.env.SERVICE}/proprietary.public.sqlite`))
+  sqlite_vec.load(new Database(`./datastores/${Bun.env.SERVICE}/proprietary.private.sqlite`))
+
+  new Database(`datastores/${Bun.env.SERVICE}/datastore.sqlite`).run(`
+    CREATE TABLE IF NOT EXISTS telemetry
+      (
+        conv_id     TEXT,
+        config      TEXT,
+        role        TEXT,
+        timestamp   TEXT,
+        content     TEXT,
+        metadata    TEXT,
         UNIQUE(conv_id, timestamp)
-        );`)
+      );
 
-db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-        config TEXT NOT NULL,
-        email TEXT PRIMARY KEY UNIQUE NOT NULL,
-        role TEXT NOT NULL,
-        password_hash TEXT NOT NULL
-        );`)
+    CREATE TABLE IF NOT EXISTS users
+      (
+        config          TEXT NOT NULL,
+        email           TEXT PRIMARY KEY UNIQUE NOT NULL,
+        role            TEXT NOT NULL,
+        password_hash   TEXT NOT NULL
+      );
+    `)
+}
+
+await setup()
