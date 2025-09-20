91dafd7b1922e9e2d7e9b65a4512678727354d0c
Charles-Henri Arnould <charnould@icloud.com>
2025-09-20 23:00:19 +0200

    style: apply prettier

--- tests/unit/utils/chunk-markdown.test.ts
+++ tests/unit/utils/chunk-markdown.test.ts
@@ -5,7 +5,10 @@ it('should split markdown into chunks', async () => {
   const file = Bun.file('tests/unit/utils/__mocks__/markdown-file.md')
   const file_content = await file.text()
 
-  const r = await split_markdown_into_chunks({ markdown: file_content, max_tokens: 7500 })
+  const r = await split_markdown_into_chunks({
+    markdown: file_content,
+    max_tokens: 7500
+  })
 
   expect(r.length).toBe(2)
   expect(r[0]).toMatchSnapshot()
