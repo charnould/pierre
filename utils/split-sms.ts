9f40c3d76a2ee5b9bb62702471a816b48b15c87b
Charles-Henri Arnould <charnould@icloud.com>
2025-09-24 13:16:17 +0200

    fix: remove sms feature

--- utils/split-sms.ts
+++
@@ -1,9 +0,0 @@
-// Caution:
-// CM supports a maximum of 10 `parts` per SMS (1 SMS = 1 grey/green bubble in the iOS Messages app).
-// Currently, this function does not split or group by `parts`, nor does it verify SMS length`.
-// While this may NOT cause issues, be mindful if users say they receive "incomplete" response.
-export const split_sms = (sms: string) =>
-  sms
-    .split('\n')
-    .map((part) => part.replace(/\n\n$/g, '').trim())
-    .filter((part) => part !== '')
