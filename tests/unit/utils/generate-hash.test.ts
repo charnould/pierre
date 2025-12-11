6285701e9e552981ec2f40f50376c51cd8c03e8b
Charles-Henri Arnould <charnould@icloud.com>
2025-12-11 23:21:34 +0100

    test update

--- tests/unit/utils/generate-hash.test.ts
+++ tests/unit/utils/generate-hash.test.ts
@@ -8,7 +8,7 @@ it('should hash a string', async () => {
 })
 
 it('should geneate a SHA-256 hash of a file', async () => {
-  const file_to_hash = Bun.file('./assets/default/files/_metadata.xlsx')
+  const file_to_hash = Bun.file('./assets/core/files/_metadata.xlsx')
   const data = await file_to_hash.arrayBuffer()
   const hashed = '4312a19df8274b400147512d5daf2ebffadbd0ad51ad06ceb0b97af43d41070c'
   expect(checksum_file(data)).toStrictEqual(hashed)
