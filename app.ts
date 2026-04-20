e566aad05e2cbb6b56862aebaba837840f1c27a0
Charles-Henri Arnould <charnould@icloud.com>
2026-04-20 11:14:58 +0200

    refactor: use native `Bun` cronjob + reorganize imports

--- app.ts
+++ app.ts
@@ -1,33 +1,32 @@
-import { setup } from './utils/setup'
-import { authenticate } from './utils/authenticate-user'
-import { run_pipeline } from './utils/knowledge/run-pipeline'
-import { topicize, score } from './utils/analyze-conversation'
+import { setup } from "./utils/setup";
 
-import { Hono } from 'hono'
-import { CronJob } from 'cron'
-import { serveStatic } from 'hono/bun'
-import { secureHeaders } from 'hono/secure-headers'
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { secureHeaders } from "hono/secure-headers";
 
-import { controller as get_ai } from './controllers/GET.ai'
-import { controller as get_login } from './controllers/GET.login'
-import { controller as get_index } from './controllers/GET.index'
-import { controller as get_admin } from './controllers/GET.admin'
-import { controller as get_users } from './controllers/GET.users'
-import { controller as get_knowledge } from './controllers/GET.knowledge'
-import { controller as get_statistics } from './controllers/GET.statistics'
-import { controller as get_conversations } from './controllers/GET.conversations'
+import { controller as get_admin } from "./controllers/GET.admin";
+import { controller as get_ai } from "./controllers/GET.ai";
+import { controller as get_conversations } from "./controllers/GET.conversations";
+import { controller as get_index } from "./controllers/GET.index";
+import { controller as get_knowledge } from "./controllers/GET.knowledge";
+import { controller as get_login } from "./controllers/GET.login";
+import { controller as get_statistics } from "./controllers/GET.statistics";
+import { controller as get_users } from "./controllers/GET.users";
+import { controller as post_conversation } from "./controllers/POST.conversations";
+import { controller as post_knowledge } from "./controllers/POST.knowledge";
+import { controller as post_login } from "./controllers/POST.login";
+import { controller as post_users } from "./controllers/POST.users";
 
-import { controller as post_login } from './controllers/POST.login'
-import { controller as post_users } from './controllers/POST.users'
-import { controller as post_knowledge } from './controllers/POST.knowledge'
-import { controller as post_conversation } from './controllers/POST.conversations'
+import { topicize, score } from "./utils/analyze-conversation";
+import { authenticate } from "./utils/authenticate-user";
+import { run_pipeline } from "./utils/knowledge/run-pipeline";
 
 // Prepare the environment and database before starting the app:
 // 1. Create necessary directories for the current service
 // 2. Initialize SQLite databases
-await setup()
+await setup();
 
-const app = new Hono()
+const app = new Hono();
 
 // Configure the secure headers for the app.
 // This allows other websites to iframe PIERRE
@@ -36,61 +35,50 @@ app.use(
     crossOriginResourcePolicy: false,
     contentSecurityPolicy: {
       // TODO: This should be modified to allow only a few trusted domains
-      frameAncestors: ["'self'", 'http://localhost:*', '*']
-    }
-  })
-)
+      frameAncestors: ["'self'", "http://localhost:*", "*"],
+    },
+  }),
+);
 
-// Initiate cronjobs
-
-// Update knowledge database with custom content
-CronJob.from({
-  onTick: async () => await run_pipeline(),
-  cronTime: '0 0 4 * * *', // Runs every day at 4:00 AM
-  timeZone: 'Europe/Paris',
-  start: true
-})
-
-// Score conversation and assign topic with AI
-CronJob.from({
-  onTick: async () => {
-    await topicize()
-    await score()
-  },
-  cronTime: '0 0 5 * * *', // Runs every day at 5:00 AM
-  timeZone: 'Europe/Paris',
-  start: true
-})
+// Cronjob
+// Runs every day at 4:00 AM
+Bun.cron("0 4 * * *", async () => {
+  // Update knowledge database with custom content
+  await run_pipeline();
+  // Score conversation and assign topic with AI
+  await topicize();
+  await score();
+});
 
 // Serve static files from the assets directory
 // Except for the config.ts file, which should return a 404 or redirect
-app.get('/assets/:domain/config.ts', (c) => c.notFound())
-app.use('/assets/*', serveStatic({ root: './' }))
+app.get("/assets/:domain/config.ts", (c) => c.notFound());
+app.use("/assets/*", serveStatic({ root: "./" }));
 
 // AI generation routes
-app.get('/c', authenticate, get_index)
-app.get('/ai', authenticate, get_ai)
+app.get("/c", authenticate, get_index);
+app.get("/ai", authenticate, get_ai);
 
 // Admin routes
-app.get('/a/login', get_login)
-app.get('/a', authenticate, get_admin)
-app.get('/a/users', authenticate, get_users)
-app.get('/a/knowledge', authenticate, get_knowledge)
-app.get('/a/statistics', authenticate, get_statistics)
-app.get('/a/conversations', authenticate, get_conversations)
+app.get("/a/login", get_login);
+app.get("/a", authenticate, get_admin);
+app.get("/a/users", authenticate, get_users);
+app.get("/a/knowledge", authenticate, get_knowledge);
+app.get("/a/statistics", authenticate, get_statistics);
+app.get("/a/conversations", authenticate, get_conversations);
 
-app.post('/a/login', post_login)
-app.post('/a/users', authenticate, post_users)
-app.post('/a/knowledge', authenticate, post_knowledge)
-app.post('/a/conversations', authenticate, post_conversation)
+app.post("/a/login", post_login);
+app.post("/a/users", authenticate, post_users);
+app.post("/a/knowledge", authenticate, post_knowledge);
+app.post("/a/conversations", authenticate, post_conversation);
 
 // Catch-all route that redirects to a new conversation
 app.notFound(async (c) =>
-  c.redirect(`/c?config=${c.req.query('config')}&data=${c.req.query('data')}`)
-)
+  c.redirect(`/c?config=${c.req.query("config")}&data=${c.req.query("data")}`),
+);
 
 // Handle errors by returning a 404 response
-app.onError((_err, c) => c.notFound())
+app.onError((_err, c) => c.notFound());
 
 // Export the app configuration
-export default { idleTimeout: 240, fetch: app.fetch }
+export default { idleTimeout: 240, fetch: app.fetch };
