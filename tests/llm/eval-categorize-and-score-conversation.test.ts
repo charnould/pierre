9ab73302a181f084c42340eca0e335294ae33185
Charles-Henri Arnould <charnould@icloud.com>
2026-04-20 22:30:51 +0200

    refactor: remove dead code (after agentification)

--- tests/llm/eval-categorize-and-score-conversation.test.ts
+++ tests/llm/eval-categorize-and-score-conversation.test.ts
@@ -1,42 +1,42 @@
-import { afterAll, beforeAll, expect, it, setSystemTime } from 'bun:test'
-import { SQL } from 'bun'
-import { AIContext } from '../../utils/_schema'
-import { topicize, score } from '../../utils/analyze-conversation'
-import { save_reply } from '../../utils/handle-conversation'
+import { afterAll, beforeAll, expect, it, setSystemTime } from "bun:test";
+import { SQL } from "bun";
+import { AIContext } from "../../utils/_schema";
+import { topicize, score } from "../../utils/analyze-conversation";
+import { save_reply } from "../../utils/handle-conversation";
 
-const sql = new SQL(`sqlite:datastores/${Bun.env.SERVICE}/datastore.sqlite`)
+const sql = new SQL(`sqlite:datastores/${Bun.env["SERVICE"]}/datastore.sqlite`);
 
 beforeAll(async () => {
   // Simulated responses for test cases
-  await sql`DELETE FROM telemetry`
-  setSystemTime(new Date('2012-12-12T12:05:00'))
+  await sql`DELETE FROM telemetry`;
+  setSystemTime(new Date("2012-12-12T12:05:00"));
   await save_reply(
     await AIContext.parseAsync({
-      conv_id: 'c1',
-      config: 'default',
-      role: 'user',
-      content: 'Qui es-tu ?',
-      custom_data: { raw: [] }
-    })
-  )
+      conv_id: "c1",
+      config: "default",
+      role: "user",
+      content: "Qui es-tu ?",
+      custom_data: { raw: [] },
+    }),
+  );
 
-  setSystemTime(new Date('2012-12-12T12:10:00'))
+  setSystemTime(new Date("2012-12-12T12:10:00"));
   await save_reply(
     await AIContext.parseAsync({
-      conv_id: 'c1',
-      config: 'default',
-      role: 'assistant',
-      content: 'Je suis Pierre !',
-      custom_data: { raw: [] }
-    })
-  )
-})
+      conv_id: "c1",
+      config: "default",
+      role: "assistant",
+      content: "Je suis Pierre !",
+      custom_data: { raw: [] },
+    }),
+  );
+});
 
-afterAll(() => setSystemTime())
+afterAll(() => setSystemTime());
 
-it('should score conversation and assign topic with AI', async () => {
-  await score()
-  await topicize()
+it("should score conversation and assign topic with AI", async () => {
+  await score();
+  await topicize();
 
   const record = await sql`
     SELECT
@@ -47,10 +47,10 @@ it('should score conversation and assign topic with AI', async () => {
       conv_id = 'c1'
     LIMIT
       1
-  `
-  record.metadata = JSON.parse(record.metadata)
+  `;
+  record.metadata = JSON.parse(record.metadata);
 
-  expect(record.metadata.topics).toBe('chatbot')
-  expect(record.metadata.evaluation.ai.score).toBeString()
-  expect(record.metadata.evaluation.ai.comment).toBeString()
-})
+  expect(record.metadata.topics).toBe("chatbot");
+  expect(record.metadata.evaluation.ai.score).toBeString();
+  expect(record.metadata.evaluation.ai.comment).toBeString();
+});
